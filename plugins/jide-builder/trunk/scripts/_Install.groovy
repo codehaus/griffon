6f85ad9c21293929eac3a63526fe44fe0cfc834e
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2008-11-27 01:50:49 +0000

    fixed builder package in install script

--- plugins/jide-builder/trunk/scripts/_Install.groovy
+++ plugins/jide-builder/trunk/scripts/_Install.groovy
@@ -1,5 +1,5 @@
 
 //TODO config slurping and writing
 new File("${basedir}/griffon-app/conf/Builder.groovy").append("""
-root.'groovy.swing.JideBuilder'.view = '*'
+root.'griffon.builder.jide.JideBuilder'.view = '*'
 """)
