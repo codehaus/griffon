76f91969ae8819531d9133239dc46759d0f71041
shemnon <shemnon@bb902e69-0157-0410-a63b-ba7c3dd83122>
2008-12-29 14:26:34 +0000

    setClasspath event unneeded, breaks with 1.6-RC-1

--- plugins/griffon-swingx-builder/trunk/scripts/_Events.groovy
+++ plugins/griffon-swingx-builder/trunk/scripts/_Events.groovy
@@ -1,12 +1,4 @@
 
-eventSetClasspath = { rootLoader ->
-    if (getPluginDirForName('swingx-builder')?.file?.exists()) {
-        ant.fileset(dir:"${getPluginDirForName('swingx-builder').file}/lib/", includes:"*.jar").each {
-            rootLoader.addURL(it.toURL())
-        }
-    }
-}
-
 eventCopyLibsEnd = { jardir ->
     ant.fileset(dir:"${getPluginDirForName('swingx-builder').file}/lib/", includes:"*.jar").each {
         griffonCopyDist(it.toString(), jardir)
