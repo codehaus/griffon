0de42f17a88715388be0366b05bd282866989630
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2008-12-31 03:21:05 +0000

    changing release version to 0.1

---
+++ plugins/griffon-jide-builder/tags/LATEST_RELEASE/JideBuilderGriffonPlugin.groovy
@@ -0,0 +1,15 @@
+class JideBuilderGriffonPlugin {
+    def version = "0.1"
+    def canBeGlobal = false
+    def dependsOn = [:]
+
+    def author = "Andres Almiray"
+    def authorEmail = "aalmiray@users.sourceforge.net"
+    def title = "Jide Builder Plugin"
+    def description = '''\\
+Brings the Jide Builder libraries into a Griffon Build 
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://griffon.codehaus.org/JideBuilder+Plugin"
+}
