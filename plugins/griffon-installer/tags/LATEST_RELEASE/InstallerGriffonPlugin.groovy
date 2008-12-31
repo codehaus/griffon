65872f2b403d90e08f50ef42a26b65a4a2631979
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2008-12-31 03:18:58 +0000

    changing release version to 0.1

---
+++ plugins/griffon-installer/tags/LATEST_RELEASE/InstallerGriffonPlugin.groovy
@@ -0,0 +1,19 @@
+class InstallerGriffonPlugin {
+    def version = '0.1'
+    def canBeGlobal = true
+    def dependsOn = [:]
+
+    def author = "Andres Almiray"
+    def authorEmail = "aalmiray@users.sourceforge.net"
+    def title = "Allows creating installers for your Griffon application"
+    def description = '''\
+Allows creating installers for your Griffon application.
+Supported formats are:
+ - IzPack (universal)
+ - RPM (Linux)
+ - DMG (MacOSX)
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://griffon.codehaus.org/Installer+Plugin"
+}
