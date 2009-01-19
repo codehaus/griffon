71f99a60ffc7997a891c717cc5822bafdba8e1a6
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-01-19 04:24:49 +0000

    GRIFFON-19 fest plugin is now cobertura aware

---
+++ plugins/griffon-fest/tags/LATEST_RELEASE/FestGriffonPlugin.groovy
@@ -0,0 +1,14 @@
+class FestGriffonPlugin {
+    def version = '0.2'
+    def canBeGlobal = true
+    def dependsOn = [:]
+
+    def author = "Andres Almiray"
+    def authorEmail = "aalmiray@users.sourceforge.net"
+    def title = "Enables application testing with FEST"
+    def description = '''\
+Enables application testing with FEST (http://easytesting.org)
+'''
+
+    def documentation = "http://griffon.codehaus.org/Fest+Plugin"
+}
