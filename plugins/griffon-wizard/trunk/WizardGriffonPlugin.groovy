0bb5a712bdac9ea749b14033a951b20e3c91d18d
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-01-19 05:25:57 +0000

    Initial release

--- plugins/griffon-wizard/trunk/WizardGriffonPlugin.groovy
+++ plugins/griffon-wizard/trunk/WizardGriffonPlugin.groovy
@@ -6,7 +6,7 @@ class WizardGriffonPlugin {
     // TODO Fill in these fields
     def author = "Andres Almiray"
     def authorEmail = "aalmiray@users.sourceforge.net"
-    def title = "Adds a Wizard functionality to your Griffon apps"
+    def title = "Adds a Wizard facility to your Griffon apps"
     def description = '''\\
 Enables a Wizard facility based on https://wizard.dev.java.net/
 '''
