066b2a05d95f997ebef9d6f17fcaa615e1f42ae2
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-02-03 03:49:27 +0000

    griffon-scala-plugin: first release

---
+++ plugins/griffon-scala/tags/LATEST_RELEASE/ScalaGriffonPlugin.groovy
@@ -0,0 +1,16 @@
+class ScalaGriffonPlugin {
+    def version = 0.1
+    def canBeGlobal = true
+    def dependsOn = [:]
+
+    // TODO Fill in these fields
+    def author = "Andres Almiray"
+    def authorEmail = "aalmiray@users.sourceforge.net"
+    def title = "Brings the Scala language compiler and libraries"
+    def description = '''\\
+This plugin enables the usage of Scala classes in your Griffon application.
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://griffon.codehaus.org/Scala+Plugin"
+}
