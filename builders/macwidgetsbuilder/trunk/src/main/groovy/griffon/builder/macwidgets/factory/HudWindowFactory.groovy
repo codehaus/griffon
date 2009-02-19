c5568d1244435c28b7f46f22c9d0d33862bb83e9
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-02-19 03:46:50 +0000

    macwidgetsbuilder: upgraded to macwidgets 0.9.4, added hud widgets

--- builders/macwidgetsbuilder/trunk/src/main/groovy/griffon/builder/macwidgets/factory/HudWindowFactory.groovy
+++ builders/macwidgetsbuilder/trunk/src/main/groovy/griffon/builder/macwidgets/factory/HudWindowFactory.groovy
@@ -19,8 +19,12 @@ package griffon.builder.macwidgets.factory
 import java.awt.LayoutManager
 import java.awt.Component
 import java.awt.Window
-import javax.swing.JFrame
+import javax.swing.JDialog
+import javax.swing.ComboBoxModel
+import javax.swing.DefaultComboBoxModel
 import com.explodingpixels.macwidgets.HudWindow
+import com.explodingpixels.macwidgets.HudWidgetFactory
+import groovy.swing.binding.JComboBoxMetaMethods
 
 /**
  * @author Andres Almiray <aalmiray@users.sourceforge.com>
@@ -30,16 +34,16 @@ class HudWindowFactory extends AbstractFactory {
       HudWindow.metaClass.getProperty = { String name ->
          def metaProperty = HudWindow.metaClass.getMetaProperty(name)
          if(metaProperty) return metaProperty.getProperty(delegate)
-         metaProperty = JFrame.metaClass.getMetaProperty(name)
-         if(metaProperty) return metaProperty.getProperty(delegate.getJFrame())
+         metaProperty = JDialog.metaClass.getMetaProperty(name)
+         if(metaProperty) return metaProperty.getProperty(delegate.getJDialog())
          throw new MissingPropertyException(name,HudWindow)
       }
 
       HudWindow.metaClass.setProperty = { String name, value ->
          def metaProperty = HudWindow.metaClass.getMetaProperty(name)
          if(metaProperty){ metaProperty.setProperty(delegate,value); return }
-         metaProperty = JFrame.metaClass.getMetaProperty(name)
-         if(metaProperty){ metaProperty.setProperty(delegate.getJFrame(),value); return }
+         metaProperty = JDialog.metaClass.getMetaProperty(name)
+         if(metaProperty){ metaProperty.setProperty(delegate.getJDialog(),value); return }
          throw new MissingPropertyException(name,HudWindow)
       }
    }
@@ -72,5 +76,67 @@ class HudWindowFactory extends AbstractFactory {
       } catch( MissingPropertyException mpe ) {
          parent.getContentPane().add(child)
       }
+   }
+}
+
+/**
+ * @author Andres Almiray <aalmiray@users.sourceforge.com>
+ */
+class HudWidgetsFactory extends AbstractFactory {
+   private final widgetType
+
+   HudWidgetsFactory( String widgetType ) {
+      this.widgetType = widgetType
+   }
+
+   public Object newInstance( FactoryBuilderSupport builder, Object name, Object value, Map attributes )
+            throws InstantiationException, IllegalAccessException {
+      def text = value ?: attributes.remove("text")
+      text = text ?: attributes.remove("label")
+      text = text != null ? text.toString() : ""
+      return HudWidgetFactory."createHud$widgetType"(text)
+   }
+}
+
+/**
+ * @author Andres Almiray <aalmiray@users.sourceforge.com>
+ */
+class HudComboBoxFactory extends AbstractFactory {
+   public Object newInstance( FactoryBuilderSupport builder, Object name, Object value, Map attributes )
+            throws InstantiationException, IllegalAccessException {
+      def model = null
+      if( value instanceof List ) {
+         model = new DefaultComboBoxModel(value.toArray())
+      } else if( value instanceof Object[] ) {
+         model = new DefaultComboBoxModel(value)
+      } else if( value instanceof ComboBoxModel ) {
+         model = value
+      }
+
+      if( !model ) {
+         if( attributes.containsKey("items") ) {
+            value = attributes.remove("items")
+            if( value instanceof List ) {
+               model = new DefaultComboBoxModel(value.toArray())
+            } else if( value instanceof Object[] ) {
+               model = new DefaultComboBoxModel(value)
+            } else {
+               throw new RuntimeException("In $name value of items: must be a List or Object[]")
+            }
+         } else if( attributes.containsKey("model") ) {
+            value = attributes.remove("model")
+            if( value instanceof ComboBoxModel ) {
+               model = value
+            } else {
+               throw new RuntimeException("In $name value of model: must be a ComboBoxModel")
+            }
+         } else {
+            throw new RuntimeException("in $name you must define a value of type List, Object[] or ComboBoxModel; or supply a value for either items: or model:")
+         }
+      }
+
+      def comboBox = HudWidgetFactory.createHudComboBox(model)
+      JComboBoxMetaMethods.enhanceMetaClass(comboBox)
+      return comboBox
    }
 }
