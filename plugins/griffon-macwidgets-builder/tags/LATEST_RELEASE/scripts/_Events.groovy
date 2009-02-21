be86347745e4e4a18b233569005f7e64254d751e
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-02-21 04:27:43 +0000

    Release 0.2 - upgraded to MacWidgetsBuilder 0.2 (includes MacWidgets 0.9.4)

--- plugins/griffon-macwidgets-builder/tags/LATEST_RELEASE/scripts/_Events.groovy
+++
@@ -1,5 +0,0 @@
-eventCopyLibsEnd = { jardir ->
-    ant.fileset(dir:"${getPluginDirForName('macwidgets-builder').file}/lib/", includes:"*.jar").each {
-        griffonCopyDist(it.toString(), jardir)
-    }
-}
