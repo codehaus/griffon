924d505faa2c8ea6f627610fa57d0ed088c8fdc0
shemnon <shemnon@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-02-23 05:25:15 +0000

    release 0.1 with sxb 1.6

---
+++ plugins/griffon-swingx-builder/tags/LATEST_RELEASE/SwingxBuilderGriffonPlugin.groovy
@@ -0,0 +1,15 @@
+class SwingxBuilderGriffonPlugin {
+    def version = "0.1"
+    def canBeGlobal = false
+    def dependsOn = [:]
+
+    def author = "James Williams"
+    def authorEmail = "james.l.williams@gmail.com"
+    def title = "SwingX Builder Plugin"
+    def description = '''\\
+Brings the SwingX Builder libraries into a Griffon Build 
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://griffon.codehaus.org/SwingxBuilder+Plugin"
+}
