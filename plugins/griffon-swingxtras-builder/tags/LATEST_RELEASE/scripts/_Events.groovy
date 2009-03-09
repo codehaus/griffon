92b2d19e287ceb29204bea403febb5598f07e463
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-03-09 20:16:33 +0000

    Bumping version to be compatible with Griffon 0.1.0

--- plugins/griffon-swingxtras-builder/tags/LATEST_RELEASE/scripts/_Events.groovy
+++
@@ -1,6 +0,0 @@
-
-eventCopyLibsEnd = { jardir ->
-    ant.fileset(dir:"${getPluginDirForName('swingxtras-builder').file}/lib/", includes:"*.jar").each {
-        griffonCopyDist(it.toString(), jardir)
-    }
-}
