f44b78422a6a1deb27aba42cca75d547c6ea29ef
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-05-20 05:45:17 +0000

    Updated deps to jidebuilder-2.2

---
+++ plugins/griffon-jide-builder/tags/LATEST_RELEASE/scripts/_Uninstall.groovy
@@ -0,0 +1,10 @@
+//
+// This script is executed by Griffon when the plugin is uninstalled from project.
+// Use this script if you intend to do any additional clean-up on uninstall, but
+// beware of messing up SVN directories!
+//
+
+//TODO:
+// parse Builder.groovy
+// remove all nodes referenceing JideBuilder
+// re-write Builder.groovy
