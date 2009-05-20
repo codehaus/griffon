50f3a2a621159360742a0d2068b6381b3cd57564
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-05-20 06:00:45 +0000

    updaded deps to FlamingoBuilder 0.2

---
+++ plugins/griffon-flamingo-builder/tags/LATEST_RELEASE/FlamingoBuilderGriffonPlugin.groovy
@@ -0,0 +1,16 @@
+class FlamingoBuilderGriffonPlugin {
+    def version = "0.2"
+    def canBeGlobal = false
+    def dependsOn = [:]
+    def jdk = "1.6"
+
+    def author = "Andres Almiray"
+    def authorEmail = "aalmiray@users.sourceforge.net"
+    def title = "FlamingoBuilder Plugin"
+    def description = '''\\
+Brings the FlamingoBuilder libraries into a Griffon Build
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://griffon.codehaus.org/FlamingoBuilder+Plugin"
+}
