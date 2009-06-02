96853a232ee7eb70e5632e4f37f0419ed733e7b9
jshingler <jshingler@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-06-02 23:55:56 +0000

    Initial Commit of AbeilleForms-Builder

---
+++ plugins/griffon-abeilleform-builder/tags/RELEASE_0_1/AbeilleformBuilderGriffonPlugin.groovy
@@ -0,0 +1,16 @@
+class AbeilleformBuilderGriffonPlugin {
+    def version = 0.1
+    def canBeGlobal = true
+    def dependsOn = [:]
+
+    // TODO Fill in these fields
+    def author = "Jim Shingler"
+    def authorEmail = "ShinglerJim@gmail.com"
+    def title = "A Builder for Abeille Forms"
+    def description = '''\\
+This plugin installs the AbeilleFormsBuilder.  The builder helps integrate forms (".jfrm") into griffon
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://griffon.codehaus.org/AbeilleformBuilder+Plugin"
+}
