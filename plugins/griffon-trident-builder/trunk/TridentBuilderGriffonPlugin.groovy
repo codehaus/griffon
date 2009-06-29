18be2072c15988ef0271dbf6e5d05532f77f7509
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-06-29 02:26:16 +0000

    griffon-trident-buildeR: initial commit, still on 0.1-SNAPSHOT version, neither Trident nor TridentBuilder have been released officially yet

---
+++ plugins/griffon-trident-builder/trunk/TridentBuilderGriffonPlugin.groovy
@@ -0,0 +1,15 @@
+class TridentBuilderGriffonPlugin {
+    def version = "0.1-SNAPSHOT"
+    def canBeGlobal = false
+    def dependsOn = [:]
+
+    def author = "Andres Almiray"
+    def authorEmail = "aalmiray@users.sourceforge.net"
+    def title = "Trident Builder Plugin"
+    def description = '''\\
+Brings de Trident Builder libraries into a Griffon Build
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://griffon.codehaus.org/TridentBuilder+Plugin"
+}
