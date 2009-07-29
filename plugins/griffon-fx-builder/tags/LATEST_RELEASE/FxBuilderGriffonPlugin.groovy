a91fc78c255791f407a638ead1ed8d98e4dfd1e8
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-07-29 04:02:22 +0000

    griffon-fx: added dependency to lang-bridge plugin

---
+++ plugins/griffon-fx-builder/tags/LATEST_RELEASE/FxBuilderGriffonPlugin.groovy
@@ -0,0 +1,16 @@
+class FxBuilderGriffonPlugin {
+    def version = 0.2
+    def canBeGlobal = false
+    def dependsOn = ["lang-bridge": 0.1]
+
+    def author = "Andres Almiray"
+    def authorEmail = "aalmiray@user.sourceforge.net"
+    def title = "Enables JavaFX on your Griffon application"
+    def description = '''\\
+Enables JavaFX on your Griffon application. Brings the FxBuilder and dependencies libraries, enables
+the usage of the JavaFX Script compiler on your application.
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://griffon.codehaus.org/FxBuilder+Plugin"
+}
