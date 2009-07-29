cbea44e352f5d8ae1daf1b0b6dc92f2d3d0c23a0
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-07-29 00:12:26 +0000

    griffon-clojure: first cut. Compiles clojure sources under src/clojure

---
+++ plugins/griffon-clojure/trunk/scripts/_Events.groovy
@@ -0,0 +1,46 @@
+includeTargets << griffonScript("Init")
+
+eventCopyLibsEnd = { jardir ->
+    ant.fileset(dir: "${getPluginDirForName('clojure').file}/lib/", includes: "*.jar").each {
+        griffonCopyDist(it.toString(), jardir)
+    }
+}
+
+eventCompileStart = { type ->
+    if(compilingClojurePlugin()) return
+    if(type != "source") return
+    def clojuresrc = "${basedir}/src/clojure"
+    def clojuresrcdir = new File(clojuresrc)
+    if(!clojuresrcdir.exists()) return
+
+    println "  [clojure-plugin] Compiling Clojure sources to $classesDirPath"
+    try {
+        ant.pathconvert(pathsep:"", property: "clojure.compile.namespaces") {
+            fileset(dir: clojuresrc, includes: "**/*.clj")
+            chainedmapper {
+                packagemapper(from: "${clojuresrc}/*.clj", to: "*")
+                filtermapper {
+                    replacestring(from: "_", to: "-")
+                }
+            }
+        }
+        ant.path(id: "clojure.compile.classpath") {
+            path(refid: "griffon.compile.classpath")
+            path(location: classesDirPath)
+            path(location: clojuresrc)
+        }
+        ant.java(classname: "clojure.lang.Compile",
+                 classpathref: "clojure.compile.classpath") {
+            sysproperty(key: "clojure.compile.path", value: classesDirPath)
+            arg(line: ant.antProject.properties.'clojure.compile.namespaces')
+        }
+    }
+    catch (Exception e) {
+        ant.fail(message: "Could not compile Clojure sources: " + e.class.simpleName + ": " + e.message)
+    }
+}
+
+/**
+ * Detects whether we're compiling the Clojure plugin itself
+ */
+private boolean compilingClojurePlugin() { getPluginDirForName("clojure") == null }
