04b7ef0a35f73a8ed9b64e3aa750fd010ba7d434
venca <venca@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-08-05 09:34:37 +0000

    0.2 release

---
+++ plugins/griffon-gparallelizer/tags/LATEST_RELEASE/GparallelizerGriffonPlugin.groovy
@@ -0,0 +1,17 @@
+class GparallelizerGriffonPlugin {
+    def version = 0.2
+    def canBeGlobal = false
+    def dependsOn = [:]
+
+    // TODO Fill in these fields
+    def author = "Vaclav Pech"
+    def authorEmail = ""
+    def title = "GParallelizer Plugin"
+    def description = '''\\
+Adds GParallelizer jar files to the projects. More on GParallelizer at http://code.google.com/p/gparallelizer/
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://griffon.codehaus.org/Gparallelizer+Plugin"
+
+}
