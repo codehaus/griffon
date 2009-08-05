07672332d79b611551e4f8568dfe0837fa905a7d
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-08-05 04:13:09 +0000

    GfxBuilder: released and tagged 0.2

---
+++ builders/gfxbuilder/tags/GFXBUILDER_0.2/GfxPad/griffon-app/conf/Builder.groovy
@@ -0,0 +1,18 @@
+root {
+    'groovy.swing.SwingBuilder' {
+        controller = ['Threading']
+        view = '*'
+    }
+    'griffon.app.ApplicationBuilder' {
+        view = '*'
+    }
+    'griffon.builder.jide.JideBuilder' {
+        view = '*'
+    }
+}
+
+jx {
+    'groovy.swing.SwingXBuilder' {
+        view = '*'
+    }
+}
---
+++ builders/gfxbuilder/tags/GFXBUILDER_0.2/GfxPad/griffon-app/views/MainContentPane.groovy
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2007-2008 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ */
+
+import groovy.ui.ConsoleTextEditor
+import java.awt.Color
+import java.awt.Font
+import java.awt.Toolkit
+import java.awt.event.KeyEvent
+import javax.swing.KeyStroke
+
+import static java.awt.BorderLayout.*
+import javax.swing.BorderFactory as BF
+import javax.swing.JTabbedPane
+import javax.swing.ListSelectionModel
+import javax.swing.event.CaretListener
+import javax.swing.event.DocumentListener
+import static javax.swing.JSplitPane.VERTICAL_SPLIT
+import groovy.swing.j2d.GraphicsPanel
+import groovy.swing.j2d.event.*
+import ca.odell.glazedlists.swing.EventListModel
+
+rowHeader = new ScrollPaneRuler(ScrollPaneRuler.VERTICAL)
+columnHeader = new ScrollPaneRuler(ScrollPaneRuler.HORIZONTAL)
+
+emptyRowHeader = label("")
+emptyColumnHeader = label("")
+
+splitPane(id: 'splitPane', resizeWeight: 0.75F,
+      orientation: VERTICAL_SPLIT ) {
+   scrollPane( id: 'scroller', constraints: CENTER,
+               border: lineBorder(color: Color.BLACK, thickness: 1),
+               rowHeaderView: rowHeader, columnHeaderView: columnHeader ){
+      widget( new GraphicsPanel(), id: 'canvas', border: emptyBorder(0) )
+   }
+   tabbedPane( id: "tabs", constraints: CENTER, tabPlacement: JTabbedPane.BOTTOM, 
+               border: lineBorder(color:Color.BLACK, thickness:1) ) {
+      panel( title: "Source", id: "sourceTab" ) {
+         borderLayout()
+         container( new ConsoleTextEditor(), id: 'editor', border: emptyBorder(0),
+                    font: new Font( "Monospaced", Font.PLAIN, 14 ) ){
+            action(runAction)
+         }
+      }
+      panel( title: "Errors", id: "errorsTab" ) {
+         borderLayout()
+         scrollPane( border: emptyBorder(0) ) {
+            textArea( id: 'errors', border: emptyBorder(0),
+                     background: Color.WHITE, editable: false,
+                     font: new Font( "Monospaced", Font.PLAIN, 10 ),
+                     caretPosition: bind(reverse:true){ model.caretPosition },
+                     text: bind(reverse:true){ model.errors } )
+         }
+      }
+   }
+}
+
+jidePopup( id: 'popup', movable: false, resizable: false,
+   popupMenuCanceled: { evt -> editor.textEditor.requestFocus() } ) {
+   panel {
+      borderLayout()
+      scrollPane() {
+         list( id: "suggestionList",
+               selectionMode: ListSelectionModel.SINGLE_SELECTION,
+               model: new EventListModel(model.suggestions) )
+      }
+   }
+}
+popup.setDefaultFocusComponent(suggestionList)
+suggestionList.inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), "complete")
+suggestionList.actionMap.put("complete", completeAction)
+
+canvas.addGraphicsErrorListener({ evt ->
+   controller.displayError( evt.cause.localizedMessage )
+} as GraphicsErrorListener)
+
+def toolkit = Toolkit.getDefaultToolkit()
+def screen = toolkit.getScreenSize()
+rowHeader.opaque = true
+rowHeader.preferredSize = [20,screen.width as int]
+columnHeader.opaque = true
+columnHeader.preferredSize = [screen.height as int,20]
+
+canvas.addMouseListener( rowHeader )
+canvas.addMouseMotionListener( rowHeader )
+canvas.addMouseListener( columnHeader )
+canvas.addMouseMotionListener( columnHeader )
+
+inputArea = editor.textEditor
+rootElement = inputArea.document.defaultRootElement
+// attach ctrl-enter to editor
+// need to wrap in actions to keep it from being added as a component
+actions {
+   container(inputArea, font:new Font("Monospaced", Font.PLAIN, 12), border:emptyBorder(4)) {
+      action(runAction)
+   }
+}
+
+inputArea.addCaretListener({ evt ->
+   def cursorPos = inputArea.caretPosition
+   def rowNum = rootElement.getElementIndex(cursorPos) + 1
+   def rowElement = rootElement.getElement(rowNum - 1)
+   def colNum = cursorPos - rowElement.startOffset + 1
+   controller.builder.rowNumAndColNum.setText("$rowNum:$colNum")
+} as CaretListener)
+
+errors.document.addDocumentListener( { evt ->
+   if( errors.text.size() > 0 ) errors.caretPosition = 0
+} as DocumentListener)
+
+bean( model, content: bind { inputArea.text } )
+bean( model, dirty: bind { inputArea.text?.size() > 0 } )
+
+bean( undoAction, enabled: bind { editor.undoAction.enabled } )
+bean( redoAction, enabled: bind { editor.redoAction.enabled } )
+inputArea.document.addDocumentListener({ model.dirty = true } as DocumentListener)
+
+return splitPane
