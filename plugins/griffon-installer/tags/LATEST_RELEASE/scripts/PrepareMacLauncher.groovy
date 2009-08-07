693ffaa15071f0370929a7396b57dc6006e1e800
joshareed <joshareed@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-08-07 15:09:05 +0000

    Tagging the 0.4 release of the Griffon Installer plugin

---
+++ plugins/griffon-installer/tags/LATEST_RELEASE/scripts/PrepareMacLauncher.groovy
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Gant script that prepares a Mac application bundle.
+ *
+ * @author Andres Almiray
+ * @author Josh Reed
+ *
+ * @since 0.4
+ */
+
+ant.property(environment:"env")
+griffonHome = ant.antProject.properties."env.GRIFFON_HOME"
+
+includeTargets << griffonScript("_GriffonInit")
+installerPluginBase = getPluginDirForName('installer').file as String
+
+target(prepareMacLauncher: "Prepares a Mac launcher") {
+    event("PrepareMacLauncherStart", [])
+
+    installerWorkDir = "${basedir}/installer/mac"
+    binaryDir = installerWorkDir
+
+	ant.copy(file:"${installerPluginBase}/src/templates/jarbundler/JavaApplicationStub", tofile: "${binaryDir}/${griffonAppName}")
+	ant.copy(file:"${installerPluginBase}/src/templates/jarbundler/griffon.icns", tofile: "${binaryDir}/${griffonAppName}.icns")
+
+    event("PrepareMacLauncherEnd", [])
+}
+
+setDefaultTarget(prepareMacLauncher)
