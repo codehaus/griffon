88629c79a6d11350abcb9e0d31829c8f92328e9a
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-08-26 02:30:25 +0000

    Transitions plugin: first release

--- plugins/griffon-transitions/trunk/TransitionsGriffonPlugin.groovy
+++ plugins/griffon-transitions/trunk/TransitionsGriffonPlugin.groovy
@@ -2,6 +2,7 @@ class TransitionsGriffonPlugin {
     def version = "0.1"
     def canBeGlobal = false
     def dependsOn = ["trident-builder": 0.1]
+    def jdk = "1.6"
 
     def author = "Andres Almiray"
     def authorEmail = "aalmiray@users.sourceforge.net"
@@ -9,7 +10,6 @@ class TransitionsGriffonPlugin {
     def description = '''\\
 Adds animated transitions capabilities
 '''
-
 
     // URL to the plugin's documentation
     def documentation = "http://griffon.codehaus.org/Transitions+Plugin"
