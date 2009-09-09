0641e2891cec4fe7864fd80b8f482fa5d61319e5
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-09-09 04:45:44 +0000

    griffon-easyb: disabled link to cobertura

--- plugins/griffon-easyb/trunk/scripts/RunEasyb.groovy
+++ plugins/griffon-easyb/trunk/scripts/RunEasyb.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2008 the original author or authors.
+ * Copyright 2004-2009 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -47,11 +47,11 @@ target(runEasyb:"Run Easyb stories") {
     depends(checkVersion, configureProxy, clean, packageApp, parseArguments, classpath)
     checkEasybStoriesSources()
     if( !_easyb_skip ) {
-        initCobertura()
+//        initCobertura()
         loadApp()
         configureApp()
         runEasybStories()
-        finishCobertura()
+//        finishCobertura()
     }
 }
 
