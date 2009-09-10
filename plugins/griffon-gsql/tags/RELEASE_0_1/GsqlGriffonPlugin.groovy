e14339f7e80df2a71cb15e0c344ef03715831b67
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-09-10 23:42:52 +0000

    griffon-gsql: re-releasing due to firewall setings preventing full upload

---
+++ plugins/griffon-gsql/tags/RELEASE_0_1/GsqlGriffonPlugin.groovy
@@ -0,0 +1,15 @@
+class GsqlGriffonPlugin {
+    def version = '0.1'
+    def canBeGlobal = false
+    def dependsOn = [:]
+
+    def author = "Andres Almiray"
+    def authorEmail = "aalmiray@users.sourceforge.net"
+    def title = "Gsql powered DataSource"
+    def description = '''\\
+Adds Gsql powered DataSource capabilities to your Griffon application.
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://griffon.codehaus.org/Gsql+Plugin"
+}
