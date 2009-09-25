67e70e0ac8489f90bf6bdbd4044a803b3db79e69
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-09-25 20:38:21 +0000

    griffon-jung: first release

---
+++ plugins/griffon-jung/tags/LATEST_RELEASE/scripts/_Events.groovy
@@ -0,0 +1,11 @@
+includeTargets << griffonScript("Init")
+
+def eventClosure1 = binding.variables.containsKey('eventCopyLibsEnd') ? eventCopyLibsEnd : {jardir->}
+eventCopyLibsEnd = { jardir ->
+    eventClosure1(jardir)
+    if (!isPluginProject) {
+        ant.fileset(dir:"${getPluginDirForName('jung').file}/lib/", includes:"*.jar").each {
+            griffonCopyDist(it.toString(), jardir)
+        }
+    }
+}
