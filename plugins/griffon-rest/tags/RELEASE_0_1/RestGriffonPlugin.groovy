5b52aaf36632688adb17c1dc879ce978ad65cfde
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-10-14 02:11:13 +0000

    griffon-rest: first release

---
+++ plugins/griffon-rest/tags/RELEASE_0_1/RestGriffonPlugin.groovy
@@ -0,0 +1,15 @@
+class RestGriffonPlugin {
+    def version = 0.1
+    def canBeGlobal = false
+    def dependsOn = [:]
+
+    def author = "Andres Almiray"
+    def authorEmail = "aalmiray@users.sourceforge.net"
+    def title = "REST client libraries"
+    def description = '''\\
+Adds REST capabilities to your Griffon application.
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://griffon.codehaus.org/Rest+Plugin"
+}
