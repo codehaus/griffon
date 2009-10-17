65fae8f170ffb3fb771e5edeee8ef07f6a0ca71d
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-10-17 00:13:34 +0000

    griffon-jbusycomponent: initial commit

---
+++ plugins/griffon-jbusycomponent/trunk/JbusycomponentGriffonPlugin.groovy
@@ -0,0 +1,16 @@
+class JbusycomponentGriffonPlugin {
+    def version = 0.1
+    def canBeGlobal = false
+    def dependsOn = ["swingx-builder": "0.1"]
+
+    def author = "Andres Almiray"
+    def authorEmail = "aalmiray@users.sourceforge.net"
+    def title = "Enhance any swing components with a 'busy' state"
+    def description = '''\\
+JBusyComponent: Enhance any swing components with a "busy" state.
+http://code.google.com/p/jbusycomponent
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://griffon.codehaus.org/JBusyComponent+Plugin"
+}
