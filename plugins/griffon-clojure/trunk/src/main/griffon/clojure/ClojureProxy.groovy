006906e42095a45c457c299795afff5066579100
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-10-25 19:11:08 +0000

    griffon-clojure: synced with grails-clojure, there was a fix on ClojureProxy that properly translates values between clojure and griffon calls

--- plugins/griffon-clojure/trunk/src/main/griffon/clojure/ClojureProxy.groovy
+++ plugins/griffon-clojure/trunk/src/main/griffon/clojure/ClojureProxy.groovy
@@ -29,12 +29,40 @@ class ClojureProxy {
         return new ClojureProxy(ns: ns)
     }
 
+    def convertArg(Closure fn) {
+        fn as clojure.lang.IFn
+    }
+
+    def convertArg(Map mp) {
+        clojure.lang.PersistentHashMap.create(mp)
+    }
+
+    def convertArg(arg) {
+        arg
+    }
+
+    def convertArgs(args) {
+        args.collect {
+            convertArg(it)
+        } as Object[]
+    }
+    
     def methodMissing(String name, args) {
         def impl = { Object[] a = new Object[0] ->
             def var = RT.var(delegate.ns, name)
-            var.invoke (*a)
+            var.invoke (*convertArgs(a))
         }
         ClojureProxy.metaClass."${name}" = impl
         impl(args)
+    }
+
+    def propertyMissing(String name) {
+        def impl = { -> RT.var(ns, name)?.get() }
+        def getterName = "get${name[0].toUpperCase()}"
+        if(name.size() > 1) {
+            getterName += name[1..-1]
+        }
+        ClojureProxy.metaClass."${getterName}" = impl
+        impl()
     }
 }
