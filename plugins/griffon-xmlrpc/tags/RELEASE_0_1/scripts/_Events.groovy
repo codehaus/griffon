31a2e9e4053f878c3a79c33abc39ca21a22de8b1
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-10-26 16:55:11 +0000

    griffon-xmlrpc: first release

---
+++ plugins/griffon-xmlrpc/tags/RELEASE_0_1/scripts/_Events.groovy
@@ -0,0 +1,9 @@
+def eventClosure1 = binding.variables.containsKey('eventCopyLibsEnd') ? eventCopyLibsEnd : {jardir->}
+eventCopyLibsEnd = { jardir ->
+    eventClosure1(jardir)
+    if (!isPluginProject) {
+        ant.fileset(dir:"${getPluginDirForName('xmlrpc').file}/lib/", includes:"*.jar").each {
+            griffonCopyDist(it.toString(), jardir)
+        }
+    }
+}
