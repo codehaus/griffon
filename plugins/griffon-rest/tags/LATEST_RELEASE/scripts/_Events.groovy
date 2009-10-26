f488669a1327932628d06674456787d2d28677b3
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-10-26 17:11:48 +0000

    griffon-rest: new release with updated dependencies

---
+++ plugins/griffon-rest/tags/LATEST_RELEASE/scripts/_Events.groovy
@@ -0,0 +1,7 @@
+eventCopyLibsEnd = { jardir ->
+    if (!isPluginProject) {
+        ant.fileset(dir:"${getPluginDirForName('rest').file}/lib/", includes:"*.jar").each {
+            griffonCopyDist(it.toString(), jardir)
+        }
+    }
+}
