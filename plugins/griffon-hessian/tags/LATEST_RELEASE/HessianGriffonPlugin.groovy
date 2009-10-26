e5a2eea4a022b0b17e6736c5984e11f7cc67d625
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-10-26 17:22:32 +0000

    griffon-hessian: first release

---
+++ plugins/griffon-hessian/tags/LATEST_RELEASE/HessianGriffonPlugin.groovy
@@ -0,0 +1,15 @@
+class HessianGriffonPlugin {
+    def version = "0.1"
+    def canBeGlobal = false
+    def dependsOn = [:]
+
+    def author = "Andres Almiray"
+    def authorEmail = "aalmiray@users.sourceforge.net"
+    def title = "Hessian client & libraries"
+    def description = '''\\
+Hessian client & libraries.
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://griffon.codehaus.org/Hessian+Plugin"
+}
