9eae474549e3c0747a7c1418b6b7d64ff117e488
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-10-27 03:36:03 +0000

    griffon-code-coverage: made sure the plugin works with Griffon 0.2

---
+++ plugins/griffon-code-coverage/tags/LATEST_RELEASE/CodeCoverageGriffonPlugin.groovy
@@ -0,0 +1,16 @@
+class CodeCoverageGriffonPlugin {
+    def version = "0.3.1"
+    def canBeGlobal = true
+    def dependsOn = [:]
+
+    def author = "Andres Almiray"
+    def authorEmail = "aalmiray@users.sourceforge.net"
+    def title = "Generates Code Coverage reports"
+    def description = """
+Creates Code Coverage reports for your code.
+Based on grails-code-coverage by Mike Hugo.
+    """
+
+    // URL to the plugin's documentation
+    def documentation = "http://griffon.codehaus.org/CodeCoverage+Plugin"
+}
