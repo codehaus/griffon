f3fd9c89b05f3a6aef1e031e4c7adfd4a9feefe0
joshareed <joshareed@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-11-05 15:32:48 +0000

    Initial release of the Griffon mail plugin

---
+++ plugins/griffon-mail/tags/RELEASE_0_1/MailGriffonPlugin.groovy
@@ -0,0 +1,16 @@
+class MailGriffonPlugin {
+    def version = 0.1
+    def canBeGlobal = false
+    def dependsOn = [:]
+
+    // TODO Fill in these fields
+    def author = "Josh Reed"
+    def authorEmail = "jareed@andrill.org"
+    def title = "Send email from your Griffon app"
+    def description = '''\\
+Send email from your Griffon app
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://griffon.codehaus.org/Mail+Plugin"
+}
