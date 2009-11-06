aa3a9cc153d214a48563ca400a096f9eb8cb20db
hamletdrc <hamletdrc@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-11-06 21:09:49 +0000

    "Adding the FontJiggler 0.1 release"

---
+++ plugins/griffon-font-jiggler/tags/RELEASE_0_1/FontJigglerGriffonPlugin.groovy
@@ -0,0 +1,17 @@
+class FontJigglerGriffonPlugin {
+    def version = 0.1
+    def canBeGlobal = false
+    def dependsOn = [:]
+
+    // TODO Fill in these fields
+    def author = "Hamlet D'Arcy"
+    def authorEmail = "hamletdrc@gmail.com"
+    def title = "FontJiggler - Adds a Font API to Swing Components"
+    def description = '''\\
+This plugin provides a useful Font API for components with fonts. 
+Currently, JTextArea is supported. 
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://griffon.codehaus.org/FontJiggler+Plugin"
+}
