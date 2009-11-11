980faa3a385e78d734fd2c6d421a53e717f6c33d
hamletdrc <hamletdrc@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-11-11 21:57:20 +0000

    upgraded font-jiggler to 0.2 a new and better API

---
+++ plugins/griffon-font-jiggler/tags/LATEST_RELEASE/FontJigglerGriffonPlugin.groovy
@@ -0,0 +1,13 @@
+class FontJigglerGriffonPlugin {
+    def version = 0.2
+    def canBeGlobal = false
+    def dependsOn = [:]
+
+    def author = "Hamlet D'Arcy"
+    def authorEmail = "hamletdrc@gmail.com"
+    def title = "FontJiggler - Adds a Font API to Swing Components"
+    def description = '''This plugin provides a useful Font API for components with fonts. '''
+
+    // URL to the plugin's documentation
+    def documentation = "http://griffon.codehaus.org/FontJiggler+Plugin"
+}
