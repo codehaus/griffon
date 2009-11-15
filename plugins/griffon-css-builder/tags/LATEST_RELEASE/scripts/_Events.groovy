573988bfc2b1501aa69d4511447f001b19b31965
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-11-15 19:40:44 +0000

    cssbuilder: Groovy expressions can be evaluated as values of CSS rules

--- plugins/griffon-css-builder/tags/LATEST_RELEASE/scripts/_Events.groovy
+++
@@ -1,5 +0,0 @@
-eventCopyLibsEnd = { jardir ->
-    ant.fileset(dir:"${getPluginDirForName('css-builder').file}/lib/", includes:"*.jar").each {
-        griffonCopyDist(it.toString(), jardir)
-    }
-}
