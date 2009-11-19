4f236cd724cf1614a0c9aeb5ab688eb8bf08c747
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-11-19 03:12:29 +0000

    griffon-protobuf: tweaked classes generation. Added missing event handlers (clean/stats/copyLibs)

--- plugins/griffon-protobuf/trunk/scripts/_Events.groovy
+++ plugins/griffon-protobuf/trunk/scripts/_Events.groovy
@@ -7,4 +7,20 @@ eventCompileStart = { type ->
     protoc()
 }
 
+eventCleanEnd = {
+    ant.delete(dir: "${projectWorkDir}/gensrc", quiet: false)
+}
+
+eventCopyLibsEnd = { jardir ->
+    ant.fileset(dir: "${getPluginDirForName('protobuf').file}/lib", includes: "*.jar").each {
+        griffonCopyDist(it.toString(), jardir)
+    }
+}
+
+eventStatsStart = { pathToInfo ->
+    if(!pathToInfo.find{it.path == "src.protobuf"} ) {
+        pathToInfo << [name: "Protobuf Sources", path: "src.protobuf", filetype: [".proto"]]
+    }
+}
+
 private boolean compilingProtobufPlugin() { getPluginDirForName("protobuf") == null }
