69dba0530ca664300e073ddc225df4494abd75e0
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-12-10 05:05:12 +0000

    griffon-gfx-builder: updagraded to gfxbuilder 0.2.3

---
+++ plugins/griffon-gfx-builder/tags/LATEST_RELEASE/GfxBuilderGriffonPlugin.groovy
@@ -0,0 +1,16 @@
+class GfxBuilderGriffonPlugin {
+    def version = "0.2.3"
+    def canBeGlobal = false
+    def dependsOn = [:]
+    def jdk = "1.6"
+
+    def author = "Andres Almiray"
+    def authorEmail = "aalmiray@users.sourceforge.net"
+    def title = "Gfx Builder Plugin"
+    def description = '''\\
+Brings de GfxBuilder libraries into a Griffon application
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://griffon.codehaus.org/GfxBuilder+Plugin"
+}
