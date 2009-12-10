b8bddb906b9bd5a80f95f3718c66c2bbe6355869
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-12-10 03:45:54 +0000

    griffon-gfx-builder: updagraded to gfxbuilder 0.2.3

---
+++ plugins/griffon-gfx-builder/tags/LATEST_RELEASE/scripts/_Install.groovy
@@ -0,0 +1,29 @@
+//
+// This script is executed by Griffon after plugin was installed to project.
+// This script is a Gant script so you can use all special variables provided
+// by Gant (such as 'baseDir' which points on project base dir). You can
+// use 'ant' to access a global instance of AntBuilder
+//
+// For example you can create directory under project tree:
+//
+//    ant.mkdir(dir:"${basedir}/griffon-app/jobs")
+//
+
+includeTargets << griffonScript("_GriffonInit")
+
+// check to see if we already have a GfxBuilder
+ConfigSlurper configSlurper = new ConfigSlurper()
+o = configSlurper.parse(new File("${basedir}/griffon-app/conf/Builder.groovy").toURL())
+boolean builderIsSet
+o.each() { prefix, v ->
+    v.each { builder, views ->
+        builderIsSet = builderIsSet || 'griffon.builder.gfx.GfxBuilder' == builder
+    }
+}
+
+if (!builderIsSet) {
+    println 'Adding GfxBuilder to Builders.groovy'
+    new File("${basedir}/griffon-app/conf/Builder.groovy").append("""
+root.'griffon.builder.gfx.GfxBuilder'.view = '*'
+""")
+}
