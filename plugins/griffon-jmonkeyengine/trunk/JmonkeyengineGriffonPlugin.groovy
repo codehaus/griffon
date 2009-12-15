afc278e5064d235abf8ab8c65dc693fecd9c292e
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-12-15 19:48:09 +0000

    griffon-jmonkeyengine: first commit

---
+++ plugins/griffon-jmonkeyengine/trunk/JmonkeyengineGriffonPlugin.groovy
@@ -0,0 +1,15 @@
+class JmonkeyengineGriffonPlugin {
+    def version = 0.1
+    def canBeGlobal = false
+    def dependsOn = [jogl: 0.1, lwjgl: 0.1]
+
+    def author = "Andres Almiray"
+    def authorEmail = "aalmiray@users.sourceforge.net"
+    def title = "JMonkeyEngine support"
+    def description = '''\\
+JMonkeyEngine support.
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://griffon.codehaus.org/Jmonkeyengine+Plugin"
+}
---
+++ plugins/griffon-jmonkeyengine/trunk/scripts/_Events.groovy
@@ -0,0 +1,35 @@
+packagingType = ''
+eventPackageStart = { type ->
+    packagingType = type
+}
+
+def eventClosure1 = binding.variables.containsKey('eventCopyLibsEnd') ? eventCopyLibsEnd : {jardir->}
+eventCopyLibsEnd = { jardir ->
+    eventClosure1(jardir)
+    if (!isPluginProject) {
+        ant.fileset(dir: "${getPluginDirForName('jmonkeyengine').file}/lib", includes: "*.jar").each {
+            griffonCopyDist(it.toString(), jardir)
+        }
+        if(buildconfig?.jmonkeyengine?.collada?.include) {
+            ant.fileset(dir: "${getPluginDirForName('jmonkeyengine').file}/lib/collada", includes: "*.jar").each {
+                griffonCopyDist(it.toString(), jardir)
+            }
+        }
+        if(buildconfig?.jmonkeyengine?.swt?.include) {
+            ant.fileset(dir: "${getPluginDirForName('jmonkeyengine').file}/lib/swt", includes: "*.jar").each {
+                griffonCopyDist(it.toString(), jardir)
+            }
+        }
+
+/*
+        if(!(packagingType in ['applet', 'webstart'])) {
+            ant.fileset(dir: "${getPluginDirForName('jmonkeyengine').file}/lib", includes: "*.jar").each {
+                griffonCopyDist(it.toString(), jardir)
+            }
+            def jmonkeyengineLibDir = "${getPluginDirForName('jmonkeyengine').file}/lib".toString()
+            copyPlatformJars(jmonkeyengineLibDir, jardir)
+            copyNativeLibs(jmonkeyengineLibDir, jardir)
+        }
+    }
+*/
+}
