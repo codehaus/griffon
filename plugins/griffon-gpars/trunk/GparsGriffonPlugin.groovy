3542fa8255b6f9ead0c38141a04bb404ea89c72a
venca <venca@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-12-21 20:53:54 +0000

    Filled trunk

---
+++ plugins/griffon-gpars/trunk/GparsGriffonPlugin.groovy
@@ -0,0 +1,15 @@
+class GparsGriffonPlugin {
+    def version = 0.1
+    def canBeGlobal = false
+    def dependsOn = [:]
+
+    def author = "Vaclav Pech"
+    def authorEmail = "vaclav@vaclavpech.eu"
+    def title = "GPars Plugin"
+    def description = '''\\
+Adds GPars jar files to the projects. More on GPars at http://gpars.codehaus.oeg/
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://griffon.codehaus.org/Gpars+Plugin"
+}
