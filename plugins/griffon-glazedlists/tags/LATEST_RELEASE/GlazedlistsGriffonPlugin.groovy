94ab93b7b7f39a72d3d35cc1ae03a7c1b9f37f28
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-12-26 23:06:41 +0000

    griffon-glazedlists: fixed GRIFFON-126 -> VerifyError when using DefualtAdvancedTableFormat

---
+++ plugins/griffon-glazedlists/tags/LATEST_RELEASE/GlazedlistsGriffonPlugin.groovy
@@ -0,0 +1,15 @@
+class GlazedlistsGriffonPlugin {
+    def version = 0.2
+    def canBeGlobal = false
+    def dependsOn = [:]
+
+    def author = "Andres Almiray"
+    def authorEmail = "aalmiray@users.sourceforge.net"
+    def title = "GlazedLists library and components"
+    def description = '''\\
+Adds GlazedLists support to your Griffon application.
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://griffon.codehaus.org/Glazedlists+Plugin"
+}
