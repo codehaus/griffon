614dd0771b52af1af9fee75e40914649d6602819
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-12-27 02:31:04 +0000

    griffon-lwjgl: updated jnlp resource injection

--- plugins/griffon-lwjgl/trunk/scripts/_Events.groovy
+++ plugins/griffon-lwjgl/trunk/scripts/_Events.groovy
@@ -1,5 +1,7 @@
 import org.codehaus.griffon.util.BuildSettings
 
+includeTargets << griffonScript("_GriffonSettings")
+
 packagingType = ''
 eventPackageStart = { type ->
     packagingType = type
@@ -34,10 +36,23 @@ eventCopyLibsEnd = { jardir ->
 }
 
 doWithPlatform = { platformOs ->
+println platformOs
+try {
     ant.fileset(dir: "${getPluginDirForName('lwjgl').file}/lib/webstart", includes: "*${platformOs}.jar").each {
         griffonCopyDist(it.toString(), new File(jardir.toString(), 'webstart').absolutePath)
     }
-    if(!config?.griffon?.extensions?.resources) config.griffon.extensions.resources = []
+    if(!config?.griffon?.extensions?.resources) config.griffon.extensions.resources = new ConfigObject()
+    def rs = config.griffon.extensions.resources[platformOs]
+    if(!rs) {
+        def co = new ConfigObject()
+        co.nativelibs = config.lwjgl.jnlp.resources.find{it.os == platformOs}.nativelibs
+        config.griffon.extensions.resources[platformOs] = co
+    } else {
+        if(!rs.nativeLibs) rs.nativeLibs = [] 
+        rs.nativeLibs.addAll(nativeLibs)
+    }
+println config.griffon.extensions.resources
+/*
     def rs = config.griffon.extensions.resources.find{ it.os == PLATFORMS[platformOs].webstartName}
     if(!rs) {
         def nativelibs = config.lwjgl.jnlp.resources.find{it.os == platformOs}.nativelibs
@@ -49,4 +64,6 @@ doWithPlatform = { platformOs ->
        if(!rs.nativeLibs) rs.nativeLibs = [] 
        rs.nativeLibs.addall(nativeLibs)
     }
+*/
+}catch(x){x.printStackTrace()}
 }
