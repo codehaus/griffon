c90c957159c49fb3454cb7935e8d8a7a76eadf6f
jwill <jwill@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-01-15 04:27:12 +0000

    Re-adding addon file that griffon release-plugin didn't catch.

---
+++ plugins/griffon-mongo-db/trunk/MongoDBGriffonAddon.groovy
@@ -0,0 +1,31 @@
+import java.util.ArrayList
+import com.mongodb.Mongo
+import com.google.inject.*
+import com.mongodb.griffon.*
+import griffon.util.IGriffonApplication
+import org.codehaus.griffon.util.BuildSettings
+
+class MongoDBGriffonAddon {
+	private IGriffonApplication application
+
+    // lifecycle methods
+
+    // called once, after the addon is created
+    def addonInit(app) {
+		application = app
+		app.addApplicationEventListener(this)    
+	}
+
+    // called once, after all addons have been inited
+    //def addonPostInit(app) {
+    //}
+
+    // called many times, after creating a builder
+    //def addonInit(app) {
+    //}
+
+    // called many times, after creating a builder and after
+    // all addons have been inited
+    //def addonPostInit(app) {
+    //}
+}
