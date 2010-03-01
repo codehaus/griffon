232e5dd2fed7aa522cea276ac74eafbcb6e5ea2d
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-03-01 02:19:41 +0000

    griffon-flamingo-builder: upgrade to griffon 0.3

--- plugins/griffon-flamingo-builder/tags/LATEST_RELEASE/scripts/_Events.groovy
+++
@@ -1,6 +0,0 @@
-
-eventCopyLibsEnd = { jardir ->
-    ant.fileset(dir:"${getPluginDirForName('flamingo-builder').file}/lib/", includes:"*.jar").each {
-        griffonCopyDist(it.toString(), jardir)
-    }
-}
