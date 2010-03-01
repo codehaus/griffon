856fc9a3ddbc10b75f2d09912465ca369fd7aaef
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-03-01 04:00:24 +0000

    griffon-scala: upgrade to griffon 0.3

---
+++ plugins/griffon-scala/tags/RELEASE_0_6/ScalaGriffonPlugin.groovy
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2009-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author Andres Almiray
+ */
+class ScalaGriffonPlugin {
+    def version = "0.6"
+    def griffonVersion = '0.3 > *'
+    def dependsOn = ["lang-bridge": 0.3]
+
+    def author = "Andres Almiray"
+    def authorEmail = "aalmiray@users.sourceforge.net"
+    def title = "Brings the Scala language compiler and libraries"
+    def description = '''
+This plugin enables the usage of Scala classes in your Griffon application.
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://griffon.codehaus.org/Scala+Plugin"
+}
---
+++ plugins/griffon-scala/tags/RELEASE_0_6/scripts/_Events.groovy
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2009-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author Andres Almiray
+ */
+
+includeTargets << griffonScript("Init")
+includePluginScript("scala", "_ScalaCommon")
+
+eventPackagePluginStart = { pluginName, plugin ->
+    def destFileName = "lib/check/scalacheck-tasks.jar"
+    ant.delete(dir: destFileName, quiet: false, failOnError: false)
+    ant.jar(destfile: destFileName) {
+        fileset(dir: classesDirPath) {
+            exclude(name: '_*.class')
+            exclude(name: '*GriffonPlugin.class')
+        }
+    }
+}
+
+eventCopyLibsEnd = { jardir ->
+    if(compilingScalaPlugin()) return
+    adjustScalaHome()
+    ant.echo(message: "[scala] Copying Scala jar files from ${scalaHome}/lib")
+
+    ant.fileset(dir:"${scalaHome}/lib/", includes:"*.jar").each {
+        griffonCopyDist(it.toString(), jardir)
+    }
+}
+
+eventCompileStart = { type ->
+    if(compilingScalaPlugin()) return
+    if(type != "source") return
+    compileScalaSrc()
+}
+ 
+/*
+eventTestCompileStart = { type ->
+    if(compilingScalaPlugin()) return
+    if(type != "scala") return
+    compileScalaTest()
+}
+
+eventTestPhasesStart = { phasesToRun ->
+    if("other" in phasesToRun && (new File("${basedir}/test/scala")).exists() && !("scala" in otherTests)) {
+        otherTests << "scala"
+    }
+}
+*/
+
+private boolean compilingScalaPlugin() { getPluginDirForName("scala") == null }
+
+eventStatsStart = { pathToInfo ->
+    if(!pathToInfo.find{ it.path == "src.commons"} ) {
+        pathToInfo << [name: "Common Sources", path: "src.commons", filetype: [".groovy",".java"]]
+    }
+    if(!pathToInfo.find{ it.path == "src.scala"} ) {
+        pathToInfo << [name: "Scala Sources", path: "src.scala", filetype: [".scala"]]
+    }
+}
