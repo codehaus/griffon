52745e818c750d4e9fe0958712e4caaf74831089
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-03-01 04:27:29 +0000

    griffon-trident-builder: upgrade to griffon 0.3

--- plugins/griffon-trident-builder/tags/LATEST_RELEASE/scripts/_Events.groovy
+++
@@ -1,5 +0,0 @@
-eventCopyLibsEnd = { jardir ->
-    ant.fileset(dir:"${getPluginDirForName('trident-builder').file}/lib/", includes:"*.jar").each {
-        griffonCopyDist(it.toString(), jardir)
-    }
-}
