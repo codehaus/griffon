78fb394acf2cbaf9f95436c1c6a320acd2ca3139
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-03-01 04:39:48 +0000

    griffon-wsclient: upgrade to griffon 0.3

--- plugins/griffon-wsclient/tags/LATEST_RELEASE/scripts/_Events.groovy
+++
@@ -1,9 +0,0 @@
-def eventClosure1 = binding.variables.containsKey('eventCopyLibsEnd') ? eventCopyLibsEnd : {jardir->}
-eventCopyLibsEnd = { jardir ->
-    eventClosure1(jardir)
-    if (!isPluginProject) {
-        ant.fileset(dir:"${getPluginDirForName('wsclient').file}/lib/", includes:"*.jar").each {
-            griffonCopyDist(it.toString(), jardir)
-        }
-    }
-}
