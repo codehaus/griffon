03f6d2fd6be1593a2ead1cc09df08a10317dc122
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-03-01 02:12:10 +0000

    griffon-clojure: upgrade to griffon 0.3

---
+++ plugins/griffon-clojure/tags/RELEASE_0_5/scripts/_Events.groovy
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2009-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author Andres Almiray
+ */
+
+includeTargets << griffonScript("Init")
+includePluginScript("clojure", "_ClojureCommon")
+
+eventSetClasspath = { classLoader ->
+    if(compilingClojurePlugin()) return
+
+    ant.fileset(dir: "${getPluginDirForName('clojure').file}/lib", includes: "*.jar").each {
+        if(getPluginDirForName('spring')?.file && it.toString() =~ "spring") return
+        classLoader.addURL(jar.file.toURI().toURL())
+    }
+}
+
+def eventClosure1 = binding.variables.containsKey('eventCopyLibsEnd') ? eventCopyLibsEnd : {jardir->}
+eventCopyLibsEnd = { jardir ->
+    eventClosure1(jardir)
+    if (!isPluginProject) {
+        ant.fileset(dir:"${getPluginDirForName('clojure').file}/lib/", includes:"*.jar").each {
+            if(getPluginDirForName('spring')?.file && it.toString() =~ "spring") return
+            griffonCopyDist(it.toString(), jardir)
+        }
+    }
+}
+
+eventCompileStart = { type ->
+    if(compilingClojurePlugin()) return
+    if(type != "source") return
+    compileClojureSrc()
+}
+
+/**
+ * Detects whether we're compiling the Clojure plugin itself
+ */
+private boolean compilingClojurePlugin() { getPluginDirForName("clojure") == null }
+
+eventStatsStart = { pathToInfo ->
+    if(!pathToInfo.find{ it.path == "src.commons"} ) {
+        pathToInfo << [name: "Common Sources", path: "src.commons", filetype: [".groovy",".java"]]
+    }
+    // TODO -- match multiline comments -> (comment ...)
+    if(!pathToInfo.find{ it.path == "src.clojure"} ) {
+        def EMPTY = /^\s*$/
+        pathToInfo << [name: "Clojure Sources", path: "src.clojure", filetype: [".clj"], locmatcher: {file ->
+            def loc = 0
+            file.eachLine { line ->
+                if(line ==~ EMPTY || line ==~ /^\s*\;.*/) return
+                loc++
+            }
+            loc
+        }]
+    }
+}
