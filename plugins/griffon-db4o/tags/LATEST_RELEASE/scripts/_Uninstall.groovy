f09bdf7fe77325c518a081f237cb252f145e4c58
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-03-01 02:17:31 +0000

    griffon-db4o: first release

---
+++ plugins/griffon-db4o/tags/LATEST_RELEASE/scripts/_Uninstall.groovy
@@ -0,0 +1,38 @@
+/* --------------------------------------------------------------------
+   griffon-db4o plugin
+   Copyright (C) 2010 Andres Almiray
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this library; if not, see <http://www.gnu.org/licenses/>.
+   ---------------------------------------------------------------------
+*/
+
+/**
+ * @author Andres Almiray
+ */
+
+// check to see if we already have a Db4oGriffonAddon
+ConfigSlurper configSlurper1 = new ConfigSlurper()
+def slurpedBuilder1 = configSlurper1.parse(new File("$basedir/griffon-app/conf/Builder.groovy").toURL())
+boolean addonIsSet1
+slurpedBuilder1.each() { prefix, v ->
+    v.each { builder, views ->
+        addonIsSet1 = addonIsSet1 || 'Db4oGriffonAddon' == builder
+    }
+}
+
+if(addonIsSet1) {
+    def builderConfigFile1 = new File("${basedir}/griffon-app/conf/Builder.groovy")
+    builderConfigFile1.text = builderConfigFile1.text - "root.'Db4oGriffonAddon'.addon=true\n"
+}
+
---
+++ plugins/griffon-db4o/tags/LATEST_RELEASE/src/templates/artifacts/Db4oConfig.groovy
@@ -0,0 +1,19 @@
+dataSource {
+}
+environments {
+    development {
+        dataSource {
+            name = "devDb.yarv"
+        }
+    }
+    test {
+        dataSource {
+            name = "testDb.yarv"
+        }
+    }
+    production {
+        dataSource {
+            name = "prodDb.yarv"
+        }
+    }
+}
