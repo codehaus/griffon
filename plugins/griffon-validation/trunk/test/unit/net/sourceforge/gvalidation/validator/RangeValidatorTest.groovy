bd27dad0c8981c3e8a4d9f8107a429197cff6e7b
aucifer_nick <aucifer_nick@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-05-13 20:35:17 +0000

    validated bug #2983281

--- plugins/griffon-validation/trunk/test/unit/net/sourceforge/gvalidation/validator/RangeValidatorTest.groovy
+++ plugins/griffon-validation/trunk/test/unit/net/sourceforge/gvalidation/validator/RangeValidatorTest.groovy
@@ -7,17 +7,25 @@ import net.sourceforge.gvalidation.validator.RangeValidator
  */
 class RangeValidatorTest extends GroovyTestCase {
 
-    public void testRangeValidation(){
+    public void testRangeValidation() {
         RangeValidator range = new RangeValidator(this)
 
         assertTrue("Should be valid", range.call(5, this, 0..10))
         assertFalse("Should not be valid", range.call(5, this, 0..<5))
 
         def now = new Date()
-        assertTrue("Should be valid", range.call(now+5, this, now..now+10))
-        assertFalse("Should not be valid", range.call(now+5, this, now..now+3))
+        assertTrue("Should be valid", range.call(now + 5, this, now..now + 10))
+        assertFalse("Should not be valid", range.call(now + 5, this, now..now + 3))
 
         assertFalse("Should not be valid", range.call(now, this, now))
+    }
+
+    // tracker bug #2983281
+    public void testRangeValidationWithInt() {
+        RangeValidator range = new RangeValidator(this)
+
+        assertTrue("Should be valid", range.call(5, this, 1..65535))
+        assertFalse("Should not be valid", range.call(-5, this, 1..65535))  
     }
 
 }
