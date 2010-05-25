b602977abe5c38ab5ad2f36fb39f662489d7dd7d
aucifer_nick <aucifer_nick@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-05-25 21:00:46 +0000

    Finally fixed release issue with validation plugin, releasing one last time

---
+++ plugins/griffon-validation/tags/RELEASE_0_3/src/templates/artifacts/Constraint.groovy
@@ -0,0 +1,15 @@
+@artifact.package@class @artifact.name@ {
+
+    /**
+     * Generated message
+     *
+     * @param propertyValue value of the property to be validated
+     * @param bean object owner of the property
+     * @param parameter configuration parameter for the constraint
+     * @return true if validation passes otherwise false
+     */
+    def validate(propertyValue, bean, parameter) {
+        // insert your custom constraint logic
+    }
+
+}
