bfb2cdac8e3680e335db1ca73dac7d6a9a5159dd
aucifer_nick <aucifer_nick@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-05-25 18:39:06 +0000

    added some javadoc in the template

--- plugins/griffon-validation/trunk/src/templates/artifacts/Constraint.groovy
+++ plugins/griffon-validation/trunk/src/templates/artifacts/Constraint.groovy
@@ -1,10 +1,15 @@
 @artifact.package@class @artifact.name@ {
 
-    static def message = "@artifact.name@.message"
-    static def parameterType = Boolean
-
+    /**
+     * Generated message
+     *
+     * @param propertyValue value of the property to be validated
+     * @param bean object owner of the property
+     * @param parameter configuration parameter for the constraint
+     * @return true if validation passes otherwise false
+     */
     def validate(propertyValue, bean, parameter) {
-        // insert your custom constraint logic 
+        // insert your custom constraint logic
     }
 
 }
