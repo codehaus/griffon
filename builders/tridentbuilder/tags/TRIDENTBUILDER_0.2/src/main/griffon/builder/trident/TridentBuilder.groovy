b6afe5463d6fa49752ce360338bc0283c7e9ac71
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-05-25 07:20:02 +0000

    tridentbuilder: tag and release 0.2

---
+++ builders/tridentbuilder/tags/TRIDENTBUILDER_0.2/src/main/griffon/builder/trident/TridentBuilder.groovy
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2009-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package griffon.builder.trident
+
+import griffon.builder.trident.factory.*
+
+import org.pushingpixels.trident.*
+import org.pushingpixels.trident.ease.*
+
+/**
+ * @author Andres Almiray
+ */
+public class TridentBuilder extends FactoryBuilderSupport {
+   public static final String DELEGATE_PROPERTY_OBJECT_ID = "_delegateProperty:id";
+   public static final String DEFAULT_DELEGATE_PROPERTY_OBJECT_ID = "id";
+
+   public TridentBuilder( boolean init = true ) {
+      super( init )
+      this[DELEGATE_PROPERTY_OBJECT_ID] = DEFAULT_DELEGATE_PROPERTY_OBJECT_ID
+   }
+
+   // taken from groovy.swing.SwingBuilder
+   public static objectIDAttributeDelegate(def builder, def node, def attributes) {
+      def idAttr = builder.getAt(DELEGATE_PROPERTY_OBJECT_ID) ?: DEFAULT_DELEGATE_PROPERTY_OBJECT_ID
+      def theID = attributes.remove(idAttr)
+      if (theID) {
+          builder.setVariable(theID, node)
+      }
+   }
+
+   def registerTrident() {
+      registerFactory("timeline", new TimelineFactory())
+      registerFactory("timelineCallback", new TimelineCallbackFactory())
+      registerFactory("uiThreadTimelineCallback", new UIThreadTimelineCallbackFactory())
+      registerFactory("interpolatedProperty", new InterpolatedPropertyFactory())
+      registerFactory("keyFrames", new KeyFramesFactory())
+      registerFactory("keyFrame", new KeyFrameFactory())
+      registerFactory("linearEase", new EaseFactory(Linear))
+      registerFactory("sineEase", new EaseFactory(Sine))
+      registerFactory("splineEase", new SplineEaseFactory())
+      // scenarios
+      registerFactory("timelineScenario",  new TimelineScenarioFactory(TimelineScenario))
+      registerFactory("parallelScenario",  new TimelineScenarioFactory(TimelineScenario.Parallel))
+      registerFactory("sequenceScenario",  new TimelineScenarioFactory(TimelineScenario.Sequence))
+      registerFactory("rendevouzScenario", new TimelineScenarioFactory(TimelineScenario.RendezvousSequence))
+      registerFactory("timelineScenarioCallback", new TimelineScenarioCallbackFactory())
+      //
+      registerFactory("swingRepaintTimeline", new SwingRepaintTimelineFactory())
+      registerFactory("timelineRunnable", new TimelineRunnableFactory())
+   }
+}
---
+++ builders/tridentbuilder/tags/TRIDENTBUILDER_0.2/src/main/griffon/builder/trident/impl/MutableTimelineCallback.groovy
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2009-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package griffon.builder.trident.impl
+
+import org.pushingpixels.trident.Timeline.TimelineState
+import org.pushingpixels.trident.callback.*
+
+/**
+ * @author Andres Almiray
+ */
+class MutableTimelineCallback implements TimelineCallback {
+   Closure timelineStateChangedClosure
+   Closure timelinePulseClosure
+
+   private final builder
+
+   MutableTimelineCallback( FactoryBuilderSupport builder ) {
+      this.builder = builder
+   }
+
+   public void onTimelineStateChanged(TimelineState oldState,
+         TimelineState newState, float durationFraction,
+         float timelinePosition) {
+      execClosure(timelineStateChangedClosure, oldState, newState, durationFraction, timelinePosition)
+   }
+
+   public void onTimelinePulse(float durationFraction, float timelinePosition) {
+      execClosure(timelinePulseClosure, durationFraction, timelinePosition)
+   }
+
+   public void setTimelineStateChanged( Closure closure ) {
+      timelineStateChanged(closure)
+   }
+
+   public void timelineStateChanged( Closure closure ) {
+      timelineStateChangedClosure = closure
+   }
+
+   public void setTimelinePulse( Closure closure ) {
+      timelinePulse(closure)
+   }
+
+   public void timelinePulse( Closure closure ) {
+      timelinePulseClosure = closure
+   }
+
+   // =====================
+
+   private closureDelegate
+   Closure closure
+   private boolean initialized
+
+   private void init() {
+       synchronized(this) {
+	       if(initialized) return
+       }
+       initialized = true
+       closureDelegate = closure.delegate
+       closure.resolveStrategy = Closure.DELEGATE_FIRST
+       closure.delegate = this
+       closure()
+   }
+
+   private void execClosure(Closure cls, Object... args) {
+       init()
+       if(!cls) return
+       cls.resolveStrategy = Closure.DELEGATE_FIRST
+       cls.delegate = this
+       cls(*args)
+   }
+
+   def methodMissing( String name, Object value ) {
+      // try the builder first
+      try {
+	     callPropertyOrMethod(builder, name, value)
+      }catch( MissingMethodException mme ) {
+         callPropertyOrMethod(closureDelegate, name, value)
+      }
+   }
+
+   def callPropertyOrMethod(target, name, args) {	
+	   try {
+	       def cls = target.getProperty(name)
+	       if(args?.getClass()?.isArray() || args instanceof List) {
+		       return cls(*args)
+	       } else {
+		       return cls(args)
+	       }	
+	   } catch(MissingPropertyException mpe) {
+	       if(args?.getClass()?.isArray() || args instanceof List) {
+		       return target."$name"(*args)
+	       } else {
+		       return target."$name"(args)
+	       }		
+	   }
+   }
+
+   def propertyMissing( String name ) {
+      // try the builder first
+      try {
+         return builder.getProperty(name)
+      }catch( MissingPropertyException mpe ) {
+         return closureDelegate."$name"
+      }
+   }
+
+   def propertyMissing( String name, Object value ) {
+      // try the builder first
+      try {
+         builder.setProperty(name, value)
+      }catch( MissingMethodException mpe ) {
+         closureDelegate."$name" = value
+      }
+   }
+}
