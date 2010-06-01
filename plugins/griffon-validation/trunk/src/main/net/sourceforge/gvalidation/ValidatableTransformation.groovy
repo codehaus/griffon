3183010d9130aa6829afb733144f6c0aae1dccba
aucifer_nick <aucifer_nick@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-06-01 16:54:12 +0000

    implemented validtable ast skeleton

---
+++ plugins/griffon-validation/trunk/src/main/net/sourceforge/gvalidation/ValidatableTransformation.groovy
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package net.sourceforge.gvalidation
+
+import org.codehaus.groovy.transform.ASTTransformation
+import org.codehaus.groovy.ast.ASTNode
+import org.codehaus.groovy.control.SourceUnit
+import org.codehaus.groovy.control.CompilePhase
+import org.codehaus.groovy.transform.GroovyASTTransformation
+
+/**
+ * Created by nick.zhu
+ */
+@GroovyASTTransformation(phase=CompilePhase.SEMANTIC_ANALYSIS)
+class ValidatableTransformation implements ASTTransformation {
+
+    void visit(ASTNode[] astNodes, SourceUnit sourceUnit) {
+        def classNode = sourceUnit.getAST()?.classNode()
+
+        println classNode
+    }
+
+
+}
