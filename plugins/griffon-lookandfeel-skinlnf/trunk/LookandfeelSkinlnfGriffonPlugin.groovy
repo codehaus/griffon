f2807389cb1786e736487c306b5209d36aecdbb3
hackergarten <hackergarten@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-06-02 13:30:00 +0000

    griffon-lookandfeel-skinlnf: first commit

---
+++ plugins/griffon-lookandfeel-skinlnf/trunk/LookandfeelSkinlnfGriffonPlugin.groovy
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author Andres Almiray
+ */
+class LookandfeelSkinlnfGriffonPlugin {
+    def version = 0.1
+    def griffonVersion = "0.3.2 > *"
+    def dependsOn = [lookandfeel: 0.1]
+    def toolkits = ['swing']
+
+    def author = "Andres Almiray"
+    def authorEmail = "aalmiray@users.sourceforge.net"
+    def title = "Skinlnf Swing Look & Feel"
+    def description = '''
+Skinlnf Swing Look & Feel.
+http://l2fprod.com/skinlf
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://griffon.codehaus.org/Lookandfeel+Skinlnf+Plugin"
+}
+
