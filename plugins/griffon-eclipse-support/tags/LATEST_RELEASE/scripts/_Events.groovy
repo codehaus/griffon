9d6415fb658c87ed0b1e5552db56f042351ec568
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-06-05 14:56:35 +0000

    eclipse-support: minor release because of GRIFFON-203

--- plugins/griffon-eclipse-support/tags/LATEST_RELEASE/scripts/_Events.groovy
+++
@@ -1,9 +0,0 @@
-includeTargets << griffonScript("Init")
-includePluginScript('eclipse-support', 'EclipseUpdate')
-
-eventPluginInstalled = { fullPluginName ->
-    updateEclipseClasspathFile(fullPluginName)
-}
-eventPluginUninstalled = { msg ->
-    updateEclipseClasspathFile()
-}
