b7f7cbceaab8fe3b764f7139da88bb8f06dbe55f
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-06-06 16:43:34 +0000

    griffon-miglayout: initial commit

---
+++ plugins/griffon-miglayout/trunk/scripts/_Uninstall.groovy
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author Andres Almiray
+ */
+
+//
+// This script is executed by Griffon when the plugin is uninstalled from project.
+// Use this script if you intend to do any additional clean-up on uninstall, but
+// beware of messing up SVN directories!
+//
+
+// check to see if we already have a MiglayoutGriffonAddon
+ConfigSlurper configSlurper1 = new ConfigSlurper()
+def slurpedBuilder1 = configSlurper1.parse(new File("$basedir/griffon-app/conf/Builder.groovy").toURL())
+boolean addonIsSet1
+slurpedBuilder1.each() { prefix, v ->
+    v.each { builder, views ->
+        addonIsSet1 = addonIsSet1 || 'MiglayoutGriffonAddon' == builder
+    }
+}
+
+if (addonIsSet1) {
+    println 'Removing MiglayoutGriffonAddon from Builder.groovy'
+    def builderConfigFile1 = new File("${basedir}/griffon-app/conf/Builder.groovy")
+    builderConfigFile1.text = builderConfigFile1.text - "root.'MiglayoutGriffonAddon'.addon=true\n"
+}
