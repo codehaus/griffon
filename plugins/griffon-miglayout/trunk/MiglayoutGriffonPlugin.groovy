be2a43b12dc850ca8ea47b68d11b8c03d83712af
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-06-06 16:43:34 +0000

    griffon-miglayout: initial commit

---
+++ plugins/griffon-miglayout/trunk/MiglayoutGriffonPlugin.groovy
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author Andres Almiray
+ */
+class MiglayoutGriffonPlugin {
+    def version = 0.1
+    def griffonVersion = '0.3.2 > *' 
+    def dependsOn = [:]
+    def toolkits = ['swing']
+    def license = 'Apache Software License 2.0'
+
+    def author = 'Andres Almiray'
+    def authorEmail = 'aalmiray@users.sourceforge.net'
+    def title = 'MigLayout Plugin'
+    def description = '''
+MigLayout Plugin
+http://miglayout.com
+'''
+
+    def documentation = 'http://griffon.codehaus.org/Miglayout+Plugin'
+}
