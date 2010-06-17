d2970f0718b3b21b05cda2336ab6e4ee2b690140
hamletdrc <hamletdrc@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-06-17 10:37:08 +0000

    release of 0.2

---
+++ plugins/griffon-fast-translate/tags/RELEASE_0_2/griffon-app/conf/Config.groovy
@@ -0,0 +1,13 @@
+griffon {}
+
+griffon.jars.destDir='dist/addon'
+
+//griffon.jars.jarName='FastTranslateGriffonAddon.jar'
+
+// The following properties have been added by the Upgrade process...
+griffon.jars.pack=false // jars were not automatically packed in Griffon 0.0
+griffon.jars.sign=true // jars were automatically signed in Griffon 0.0
+griffon.extensions.jarUrls = [] // remote jars were not possible in Griffon 0.1
+griffon.extensions.jnlpUrls = [] // remote jars were not possible in Griffon 0.1
+
+
