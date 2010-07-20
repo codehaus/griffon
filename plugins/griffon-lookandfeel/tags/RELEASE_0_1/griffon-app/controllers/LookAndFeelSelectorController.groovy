257f3e6e622d14ac0fbc482252702ee5d6650533
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-07-20 22:03:06 +0000

    griffon-lookandfeel: first release

---
+++ plugins/griffon-lookandfeel/tags/RELEASE_0_1/griffon-app/controllers/LookAndFeelSelectorController.groovy
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import javax.swing.SwingUtilities
+import griffon.lookandfeel.LookAndFeelInfo
+import griffon.lookandfeel.LookAndFeelProvider
+import griffon.lookandfeel.LookAndFeelManager
+
+/**
+ * @author Andres Almiray
+ */
+class LookAndFeelSelectorController {
+    def model
+    def view
+
+    def preview = { evt = null ->
+        if(!model.lafSelection) return
+        LookAndFeelManager.instance.preview(model.lafSelection, view.desktop)
+    }
+
+    def reset = { evt = null ->
+        LookAndFeelProvider provider = LookAndFeelManager.instance.getLookAndFeelProvider('System')
+        LookAndFeelInfo info = LookAndFeelManager.instance.getLookAndFeelInfo(provider, 'System')
+        execAsync {
+            LookAndFeelManager.instance.installLookAndFeel(info.lookAndFeel, app)
+            setCurrentLookAndFeel(provider)
+            model.reset = true
+        }
+    }
+
+    private void setCurrentLookAndFeel(LookAndFeelProvider provider) {
+        if(!provider) return
+        model.providerSelection = provider
+
+        for(p in model.providerModel) {
+            if(p == provider) {
+                model.providerModel.selectedItem = provider
+                model.providerSelection = provider
+                break
+            }
+        }
+
+        selectCurrentLookAndFeel(provider)
+    }
+
+    private void selectCurrentLookAndFeel(LookAndFeelProvider provider) {
+        LookAndFeelInfo currentLookAndFeel
+        for(lookAndFeelInfo in provider.supportedLookAndFeels) {
+            if(lookAndFeelInfo.isCurrentLookAndFeel()) {
+                currentLookAndFeel = lookAndFeelInfo
+                break
+            }
+        }
+
+        for(l in model.lafInfoModel) {
+            if(l == currentLookAndFeel) {
+                model.lafInfoModel.selectedItem = currentLookAndFeel
+                model.lafSelection = currentLookAndFeel
+                break
+            }
+        }
+    }
+}
---
+++ plugins/griffon-lookandfeel/tags/RELEASE_0_1/src/main/griffon/lookandfeel/DefaultLookAndFeelProvider.groovy
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package griffon.lookandfeel
+
+import java.awt.Component
+import java.awt.Window
+import javax.swing.UIManager
+import javax.swing.LookAndFeel
+import javax.swing.SwingUtilities
+import griffon.core.GriffonApplication
+
+/**
+ * @author Andres Almiray
+ */
+abstract class DefaultLookAndFeelProvider extends LookAndFeelProvider {
+    DefaultLookAndFeelProvider(String name) {
+        super(name)
+    }
+    
+    void preview(griffon.lookandfeel.LookAndFeelInfo lookAndFeelInfo, Component component) {
+        if(!handles(lookAndFeelInfo)) return
+        lookAndFeelInfo.preview(component)
+    }
+
+    void apply(griffon.lookandfeel.LookAndFeelInfo lookAndFeelInfo, GriffonApplication application) {
+        if(!handles(lookAndFeelInfo)) return
+        SwingUtilities.invokeLater {
+            LookAndFeel lookAndFeel = lookAndFeelInfo.lookAndFeel
+            UIManager.setLookAndFeel(lookAndFeel)
+            for(Window window : Window.getWindows()) {
+                SwingUtilities.updateComponentTreeUI(window)
+            }
+            application.event('LookAndFeelChanged',[lookAndFeel])
+        }
+    }
+}
