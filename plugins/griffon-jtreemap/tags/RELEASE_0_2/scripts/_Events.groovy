c4eb9474033e99a76bd31e0f991876e300d8b199
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-07-23 07:39:54 +0000

    cleanup release

---
+++ plugins/griffon-jtreemap/tags/RELEASE_0_2/scripts/_Events.groovy
@@ -0,0 +1,15 @@
+
+
+def eventClosure1 = binding.variables.containsKey('eventCopyLibsEnd') ? eventCopyLibsEnd : {jardir->}
+eventCopyLibsEnd = { jardir ->
+    eventClosure1(jardir)
+    if (!isPluginProject) {
+        def pluginDir = getPluginDirForName('jtreemap')
+        if(pluginDir?.file?.exists()) {
+            ant.fileset(dir: "${pluginDir.file}/lib/", includes: '*.jar').each {
+                griffonCopyDist(it.toString(), jardir)
+            }
+        }
+    }
+}
+
