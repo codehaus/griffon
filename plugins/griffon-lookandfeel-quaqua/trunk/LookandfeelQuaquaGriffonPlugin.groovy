a40e3b5f78f178595a1e21559b2a748cabc1f3cf
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-07-29 08:35:57 +0000

    griffon-lookandfeel-quaqua: initial commit

---
+++ plugins/griffon-lookandfeel-quaqua/trunk/LookandfeelQuaquaGriffonPlugin.groovy
@@ -0,0 +1,38 @@
+/*
+ * griffon-lookandfeel-quaqua: Quaqua Look&Feel for Griffon
+ * Copyright 2010 and beyond, Andres Almiray
+ *
+ * SmartGWT is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 3
+ * as published by the Free Software Foundation.  SmartGWT is also
+ * available under typical commercial license terms - see
+ * smartclient.com/license.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ */
+
+/**
+ * @author Andres Almiray
+ */
+class LookandfeelQuaquaGriffonPlugin {
+    def version = 0.1
+    def griffonVersion = '0.9 > *'
+    def dependsOn = [lookandfeel: 0.1]
+    def toolkits = ['swing']
+    def platforms = ['macosx', 'macosx64']
+    def license = 'GNU LGPL 2.1'
+
+    def author = 'Andres Almiray'
+    def authorEmail = 'aalmiray@users.sourceforge.net'
+    def title = 'Quaqua Swing Look & Feel'
+    def description = '''
+Quaqua Swing Look & Feel.
+'''
+
+    // URL to the plugin's documentation
+    def documentation = 'http://griffon.codehaus.org/Lookandfeel+Quaqua+Plugin'
+}
+
