74ed2a16c9057b617651201e9b0e8dee578919b5
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-08-12 07:21:11 +0000

    griffon-fest: fix for GRIFFON-232

---
+++ plugins/griffon-fest/tags/RELEASE_0_4_1/src/test/griffon/fest/FestSwingTestCase.groovy
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2009-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package griffon.fest
+
+import org.fest.swing.fixture.FrameFixture
+import java.util.concurrent.CountDownLatch
+import griffon.swing.SwingApplication
+import griffon.test.GriffonUnitTestCase
+
+/**
+ * @author Andres Almiray
+ */
+abstract class FestSwingTestCase extends GriffonUnitTestCase {
+    private boolean realized = false
+    private final Object realizedLock = new Object()
+
+    protected SwingApplication app
+    protected FrameFixture window
+
+    protected final void setUp() {
+        super.setUp()
+        initApp()
+        onSetUp()
+    }
+
+    protected final void tearDown() {
+        window.cleanUp()
+        onTearDown()
+        super.tearDown()
+    }
+ 
+    protected void onSetUp() { }
+    protected void onTearDown() { }
+ 
+    private final void initApp() {
+        synchronized(realizedLock) {
+            if(!realized) {
+                app.metaClass.exit = { ->
+                    // NOOP
+                }
+
+                def latch = new CountDownLatch(1)
+                app.execSync {
+                    app.realize()
+                    latch.countDown()
+                }
+                latch.await()
+                realized = true
+            }
+        }
+        window = initWindow()
+        window.show()
+    }
+ 
+    protected FrameFixture initWindow() {
+        if(app.windowManager.windows.size() > 0) return new FrameFixture(app.windowManager.windows[0])
+        throw new IllegalStateException("Application does not have managed Windows!")
+    }
+}
