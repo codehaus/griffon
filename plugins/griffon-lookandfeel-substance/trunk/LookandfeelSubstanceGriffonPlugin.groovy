59a693f6ca3cbf323a2d9bf151db9c298fd0acd7
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-08-18 16:52:14 +0000

    griffon-lookandfeel-substance: upgraded to Substance 6.1

--- plugins/griffon-lookandfeel-substance/trunk/LookandfeelSubstanceGriffonPlugin.groovy
+++ plugins/griffon-lookandfeel-substance/trunk/LookandfeelSubstanceGriffonPlugin.groovy
@@ -18,10 +18,10 @@
  * @author Andres Almiray
  */
 class LookandfeelSubstanceGriffonPlugin {
-    def version = 0.1
+    def version = 0.2
     def griffonVersion = '0.9 > *'
-    def dependsOn = [lookandfeel: 0.1,
-                     'trident-builder': 0.4]
+    def dependsOn = [lookandfeel: 0.2,
+                     'trident-builder': 0.5]
     def toolkits = ['swing']
     def license = 'Apache Software License 2.0'
 
