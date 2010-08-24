98be877577e5330e7b513309947613c29331b5bd
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-08-24 19:23:56 +0000

    griffon-couchdb: upgraded dependencies

---
+++ plugins/griffon-couchdb/tags/RELEASE_0_3/scripts/_Install.groovy
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author Andres Almiray
+ */
+
+includeTargets << griffonScript("_GriffonInit")
+includeTargets << griffonScript("_GriffonCreateArtifacts")
+
+// check to see if we already have a CouchdbGriffonAddon
+boolean addonIsSet1
+builderConfig.each() { prefix, v ->
+    v.each { builder, views ->
+        addonIsSet1 = addonIsSet1 || 'CouchdbGriffonAddon' == builder
+    }
+}
+
+if (!addonIsSet1) {
+    println 'Adding CouchdbGriffonAddon to Builder.groovy'
+    builderConfigFile.append('''
+root.'CouchdbGriffonAddon'.addon=true
+''')
+}
+
+ant.mkdir(dir:"${basedir}/griffon-app/resources/couchdb/views")
+
+if(!(config.flatten().'griffon.couchdb.injectInto')) {
+    configFile.append('''
+griffon.couchdb.injectInto = ['controller']
+''')
+}
+
+argsMap = argsMap ?: [:]
+argsMap.skipPackagePrompt = true
+
+if(!new File("${basedir}/griffon-app/conf/CouchdbConfig.groovy").exists()) {
+   createArtifact(
+      name: "CouchdbConfig",
+      suffix: "",
+      type: "CouchdbConfig",
+      path: "griffon-app/conf")
+}
+
+if(!new File("${basedir}/griffon-app/conf/BootstrapCouchdb.groovy").exists()) {
+   createArtifact(
+      name: "BootstrapCouchdb",
+      suffix: "",
+      type: "BootstrapCouchdb",
+      path: "griffon-app/conf")
+}
