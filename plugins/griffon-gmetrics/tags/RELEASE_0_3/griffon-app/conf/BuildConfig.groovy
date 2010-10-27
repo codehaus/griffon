841e5903e6d48190fbd45dc890a237a2fb7a683b
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-10-27 23:54:25 +0000

    griffon-gmetrics:

---
+++ plugins/griffon-gmetrics/tags/RELEASE_0_3/griffon-app/conf/BuildConfig.groovy
@@ -0,0 +1,26 @@
+griffon.project.dependency.resolution = {
+    // inherit Griffon' default dependencies
+    inherits("global") {
+    }
+    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    repositories {
+        griffonPlugins()
+        griffonHome()
+        griffonCentral()
+
+        // mavenCentral()
+    }
+    dependencies {
+        // compile('org.gmetrics:GMetrics:0.3') { transitive = false }
+    }
+}
+
+griffon {
+    doc {
+        logo = '<a href="http://griffon.codehaus.org" target="_blank"><img alt="The Griffon Framework" src="../img/griffon.png" border="0"/></a>'
+        sponsorLogo = "<br/>"
+        footer = "<br/><br/>Made with Griffon (@griffon.version@)"
+    }
+}
+
+griffon.jars.destDir='target/addon'
