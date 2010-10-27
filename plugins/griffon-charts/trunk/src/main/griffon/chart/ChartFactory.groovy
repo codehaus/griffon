25dc0303968ecf7146df1931813f3447382989b2
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-10-27 18:53:27 +0000

    griffon-charts:

--- plugins/griffon-charts/trunk/src/main/griffon/chart/ChartFactory.groovy
+++ plugins/griffon-charts/trunk/src/main/griffon/chart/ChartFactory.groovy
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package griffon.charts
+package griffon.chart
 
 import com.thecoderscorner.groovychart.chart.ChartBuilder
 import org.jfree.chart.ChartPanel
--- plugins/griffon-charts/trunk/src/main/griffon/chart/GriffonChartClass.java
+++ plugins/griffon-charts/trunk/src/main/griffon/chart/GriffonChartClass.java
@@ -1,31 +1,30 @@
 /*
  * Copyright 2010 the original author or authors.
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+ */ 
+package griffon.chart;
 
-import griffon.chart.ChartFactory
-import org.codehaus.griffon.runtime.chart.ChartArtifactHandler
+import griffon.core.GriffonClass;
 
 /**
- * @author Andres.Almiray
+ * Represents a Chart class in Griffon.<p>
+ *
+ * @author Andres Almiray
  */
-class ChartsGriffonAddon {
-    def addonInit = {app ->
-        app.artifactManager.registerArtifactHandler(new ChartArtifactHandler(app))
-    }
-
-    def factories = [
-        chart: new ChartFactory()
-    ]
+public interface GriffonChartClass extends GriffonClass {
+    /** "chart" */
+    String TYPE = "chart";
+    /** "Chart" */
+    String TRAILING = "Chart";
 }
