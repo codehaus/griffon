5c2fe48e1de1a4c59194e23591cf7e553090395b
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-10-27 12:02:48 +0000

    griffon-fest:

---
+++ plugins/griffon-fest/tags/LATEST_RELEASE/src/templates/artifacts/FestTest.groovy
@@ -0,0 +1,23 @@
+@artifact.package@import org.fest.swing.fixture.*
+import griffon.fest.FestSwingTestCase
+
+class @artifact.name@ extends FestSwingTestCase {
+    // instance variables:
+    // app    - current application
+    // window - value returned from initWindow()
+    //          defaults to app.windowManager.windows[0]
+ 
+    void testSomething() {
+ 
+    }
+ 
+    // protected void onSetUp() {
+    // }
+ 
+    // protected void onTearDown() {
+    // }
+ 
+    // protected FrameFixture initWindow() {
+    //    return new FrameFixture(app.windowManager.windows[0])
+    // }
+}
