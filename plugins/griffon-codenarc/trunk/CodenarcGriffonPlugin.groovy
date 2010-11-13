b8202573ba27df30b565cc44e56f605acef78ec0
hamletdrc <hamletdrc@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-11-13 18:26:50 +0000

    Upgrade Codenarc to 0.11

--- plugins/griffon-codenarc/trunk/CodenarcGriffonPlugin.groovy
+++ plugins/griffon-codenarc/trunk/CodenarcGriffonPlugin.groovy
@@ -18,7 +18,7 @@
  * @author Andres Almiray
  */
 class CodenarcGriffonPlugin {
-    def version = 0.5
+    def version = 0.6
     def dependsOn = [:]
     def griffonVersion = '0.9 > *'
     def license = 'Apache Software License 2.0'
