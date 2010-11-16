0bbee0e2e35d8857ce9a13786601ba1e99a2af68
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-11-16 17:17:30 +0000

    griffon-harmoniccode:

---
+++ plugins/griffon-harmoniccode/tags/RELEASE_0_1/src/main/eu/hansolo/custom/Theme.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eu.hansolo.custom;
+
+/**
+ *
+ * @author hansolo
+ */
+public enum Theme
+{
+    BRIGHT,
+    DARK,
+    CUSTOM
+}
