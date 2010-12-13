9cb1dc970c201d36918e383cf002aee583979ab0
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-12-13 00:12:26 +0000

    griffon-easyb: upgrade to griffon 0.9.2

--- plugins/griffon-easyb/trunk/EasybGriffonPlugin.groovy
+++ plugins/griffon-easyb/trunk/EasybGriffonPlugin.groovy
@@ -18,9 +18,9 @@
  * @author Andres Almiray
  */
 class EasybGriffonPlugin {
-    def version = '0.3'
+    def version = '0.4'
     def dependsOn = [:]
-    def griffonVersion = '0.9 > *'
+    def griffonVersion = '0.9.2 > *'
     def license = 'Apache Software License 2.0'
 
     def author = 'Andres Almiray'
