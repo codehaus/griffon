cb75d0484b9d238e748a3108f490fc20b0866f0b
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-12-13 00:00:47 +0000

    griffon-spock: upgrade to griffon 0.9.2

--- plugins/griffon-spock/trunk/SpockGriffonPlugin.groovy
+++ plugins/griffon-spock/trunk/SpockGriffonPlugin.groovy
@@ -19,9 +19,9 @@
  */
 class SpockGriffonPlugin {
     // the plugin version
-    def version = "0.2"
+    def version = '0.3'
     // the version or versions of Griffon the plugin is designed for
-    def griffonVersion = '0.9.1 > *' 
+    def griffonVersion = '0.9.2 > *' 
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are included in plugin packaging
