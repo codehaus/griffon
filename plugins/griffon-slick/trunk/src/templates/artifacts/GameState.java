19f709e440ea3022fec8b02a97ed8ff5636e2ce7
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-12-14 00:57:53 +0000

    griffon-slick: initial commit

---
+++ plugins/griffon-slick/trunk/src/templates/artifacts/GameState.java
@@ -0,0 +1,34 @@
+@artifact.package@import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.Graphics;
+import griffon.plugins.slick.artifact.AbstractGriffonGameState;
+import java.util.Map;
+
+public class @artifact.name@ extends AbstractGriffonGameState {
+    public static final int ID = 0; // CHANGE ME!!
+    public @artifact.name.plain@Model model;
+    public @artifact.name.plain@Controller controller;
+    
+    public int getID() { return ID; } 
+
+    public void setModel(@artifact.name.plain@Model model) {
+        this.model = model;
+    }
+
+    public void setController(@artifact.name.plain@Controller controller) {
+        this.controller = controller;
+    }
+
+    public void mvcGroupInit(Map<String, ?> args) {
+        getStateBasedApp().getStateBasedGame().addState(this);
+    }
+
+    protected void doInit(GameContainer gc, StateBasedGame game) throws SlickException {
+        // handle initialization
+    }
+    
+    protected void doRender(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
+        // handle painting
+    }
+}
