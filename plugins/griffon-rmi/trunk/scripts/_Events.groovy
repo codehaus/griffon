e4f2c617834be798d66d8894fec5debbafcdf3d6
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-12-14 13:07:04 +0000

    griffon-rmi: upgrade to griffon 0.9.2

--- plugins/griffon-rmi/trunk/scripts/_Events.groovy
+++ plugins/griffon-rmi/trunk/scripts/_Events.groovy
@@ -18,16 +18,15 @@
  * @author Andres Almiray
  */
 
-def eventClosure1 = binding.variables.containsKey('eventCopyLibsEnd') ? eventCopyLibsEnd : {jardir->}
-eventCopyLibsEnd = { jardir ->
-    eventClosure1(jardir)
-    if (!isPluginProject) {
-        def pluginDir = getPluginDirForName('rmi')
-        if(pluginDir?.file?.exists()) {
-            ant.fileset(dir: "${pluginDir.file}/lib/", includes: '*.jar').each {
-                griffonCopyDist(it.toString(), jardir)
-            }
-        }
-    }
+def eventClosure1 = binding.variables.containsKey('eventSetClasspath') ? eventSetClasspath : {cl->}
+eventSetClasspath = { cl ->
+    eventClosure1(cl)
+    if(compilingPlugin('rmi')) return
+    griffonSettings.dependencyManager.flatDirResolver name: 'griffon-rmi-plugin', dirs: "${rmiPluginDir}/addon"
+    griffonSettings.dependencyManager.addPluginDependency('rmi', [
+        conf: 'compile',
+        name: 'griffon-rmi-addon',
+        group: 'org.codehaus.griffon.plugins',
+        version: rmiPluginVersion
+    ])
 }
-
