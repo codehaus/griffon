d7e7b884966b3dfbe6b190f6986e0ce4485f275b
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-12-15 03:10:34 +0000

    griffon-miglayout: upgrade to griffon 0.9.2

--- plugins/griffon-miglayout/trunk/MiglayoutGriffonPlugin.groovy
+++ plugins/griffon-miglayout/trunk/MiglayoutGriffonPlugin.groovy
@@ -18,8 +18,8 @@
  * @author Andres Almiray
  */
 class MiglayoutGriffonPlugin {
-    def version = 0.1
-    def griffonVersion = '0.9 > *' 
+    def version = 0.2
+    def griffonVersion = '0.9.2 > *' 
     def dependsOn = [:]
     def toolkits = ['swing']
     def license = 'Apache Software License 2.0'
