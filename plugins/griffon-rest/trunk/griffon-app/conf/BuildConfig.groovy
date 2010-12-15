b18337e0222b3d0158328055df0c09ebf131e848
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-12-15 01:15:21 +0000

    griffon-rest: upgrade to griffon 0.9.2

--- plugins/griffon-rest/trunk/griffon-app/conf/BuildConfig.groovy
+++ plugins/griffon-rest/trunk/griffon-app/conf/BuildConfig.groovy
@@ -1,26 +1,16 @@
 griffon.project.dependency.resolution = {
-    // inherit Griffon' default dependencies
-    inherits("global") {
-    }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    inherits("global")
     repositories {
         griffonPlugins()
         griffonHome()
         griffonCentral()
-
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        //mavenLocal()
-        //mavenCentral()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
+        mavenCentral()
+        mavenRepo name: 'Codehaus', root: 'http://repository.codehaus.org', m2compatible: true
     }
     dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-
-        // runtime 'mysql:mysql-connector-java:5.1.5'
+        compile('org.codehaus.groovy.modules.http-builder:http-builder:0.5.1') {
+            excludes 'commons-logging', 'xml-apis', 'groovy', 'log4j'
+        }
     }
 }
 
