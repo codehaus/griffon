8b0cd88d2766754c2a586bceece169da408f6693
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-12-15 01:13:33 +0000

    griffon-jabber: upgrade to griffon 0.9.2

--- plugins/griffon-jabber/trunk/scripts/_Events.groovy
+++ plugins/griffon-jabber/trunk/scripts/_Events.groovy
@@ -18,15 +18,15 @@
  * @author Andres Almiray
  */
 
-def eventClosure1 = binding.variables.containsKey('eventCopyLibsEnd') ? eventCopyLibsEnd : {jardir->}
-eventCopyLibsEnd = { jardir ->
-    eventClosure1(jardir)
-    if (!isPluginProject) {
-        def pluginDir = getPluginDirForName('jabber')
-        if(pluginDir?.file?.exists()) {
-            ant.fileset(dir: "${pluginDir.file}/lib/", includes: '*.jar').each {
-                griffonCopyDist(it.toString(), jardir)
-            }
-        }
-    }
+def eventClosure1 = binding.variables.containsKey('eventSetClasspath') ? eventSetClasspath : {cl->}
+eventSetClasspath = { cl ->
+    eventClosure1(cl)
+    if(compilingPlugin('jabber')) return
+    griffonSettings.dependencyManager.flatDirResolver name: 'griffon-jabber-plugin', dirs: "${jabberPluginDir}/addon"
+    griffonSettings.dependencyManager.addPluginDependency('jabber', [
+        conf: 'compile',
+        name: 'griffon-jabber-addon',
+        group: 'org.codehaus.griffon.plugins',
+        version: jabberPluginVersion
+    ])
 }
