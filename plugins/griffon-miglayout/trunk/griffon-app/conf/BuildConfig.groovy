ff405559e9228da8a41b83f05aaaa83489ab8f13
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-12-15 03:10:34 +0000

    griffon-miglayout: upgrade to griffon 0.9.2

--- plugins/griffon-miglayout/trunk/griffon-app/conf/BuildConfig.groovy
+++ plugins/griffon-miglayout/trunk/griffon-app/conf/BuildConfig.groovy
@@ -1,26 +1,16 @@
 griffon.project.dependency.resolution = {
-    // inherit Griffon' default dependencies
-    inherits("global") {
-    }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    inherits "global"
+    log "warn"
     repositories {
         griffonPlugins()
         griffonHome()
         griffonCentral()
-
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        //mavenLocal()
-        //mavenCentral()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
+        mavenRepo 'http://repository.sonatype.org/content/groups/public'
     }
     dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-
-        // runtime 'mysql:mysql-connector-java:5.1.5'
+        compile('com.miglayout:miglayout:3.7.3.1') {
+            excludes 'substance', 'junit'
+        }
     }
 }
 
--- plugins/griffon-miglayout/trunk/scripts/_Events.groovy
+++ plugins/griffon-miglayout/trunk/scripts/_Events.groovy
@@ -18,16 +18,16 @@
  * @author Andres Almiray
  */
 
-def eventClosure1 = binding.variables.containsKey('eventCopyLibsEnd') ? eventCopyLibsEnd : {jardir->}
-eventCopyLibsEnd = { jardir ->
-    eventClosure1(jardir)
-    if (!isPluginProject) {
-        def pluginDir = getPluginDirForName('miglayout')
-        if(pluginDir?.file?.exists()) {
-            ant.fileset(dir: "${pluginDir.file}/lib/", includes: '*.jar').each {
-                griffonCopyDist(it.toString(), jardir)
-            }
-        }
-    }
+def eventClosure1 = binding.variables.containsKey('eventSetClasspath') ? eventSetClasspath : {cl->}
+eventSetClasspath = { cl ->
+    eventClosure1(cl)
+    if(compilingPlugin('miglayout')) return
+    griffonSettings.dependencyManager.flatDirResolver name: 'griffon-miglayout-plugin', dirs: "${miglayoutPluginDir}/addon"
+    griffonSettings.dependencyManager.addPluginDependency('miglayout', [
+        conf: 'compile',
+        name: 'griffon-miglayout-addon',
+        group: 'org.codehaus.griffon.plugins',
+        version: miglayoutPluginVersion
+    ])
 }
 
