2b560fbf1eca7e989e6d2bb242e64e47a0e451ff
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-12-15 01:14:09 +0000

    griffon-xmlrpc: upgrade to griffon 0.9.2

--- plugins/griffon-xmlrpc/trunk/scripts/_Events.groovy
+++ plugins/griffon-xmlrpc/trunk/scripts/_Events.groovy
@@ -17,15 +17,16 @@
 /**
  * @author Andres ALmiray
  */
-def eventClosure1 = binding.variables.containsKey('eventCopyLibsEnd') ? eventCopyLibsEnd : {jardir->}
-eventCopyLibsEnd = { jardir ->
-    eventClosure1(jardir)
-    if (!isPluginProject) {
-        def pluginDir = getPluginDirForName('xmlrpc')
-        if(pluginDir?.file?.exists()) {
-            ant.fileset(dir: "${pluginDir.file}/lib/", includes: '*.jar').each {
-                griffonCopyDist(it.toString(), jardir)
-            }
-        }
-    }
+def eventClosure1 = binding.variables.containsKey('eventSetClasspath') ? eventSetClasspath : {cl->}
+eventSetClasspath = { cl ->
+    eventClosure1(cl)
+    if(compilingPlugin('xmlrpc')) return
+    griffonSettings.dependencyManager.flatDirResolver name: 'griffon-xmlrpc-plugin', dirs: "${xmlrpcPluginDir}/addon"
+    griffonSettings.dependencyManager.addPluginDependency('xmlrpc', [
+        conf: 'compile',
+        name: 'griffon-xmlrpc-addon',
+        group: 'org.codehaus.griffon.plugins',
+        version: xmlrpcPluginVersion
+    ])
 }
+
