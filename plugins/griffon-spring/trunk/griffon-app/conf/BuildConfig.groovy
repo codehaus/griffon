2707959940c37ba99404ef1f6bf83d89a59a97e2
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-12-16 14:34:38 +0000

    griffon-spring: more dependencies tweaks

--- plugins/griffon-spring/trunk/griffon-app/conf/BuildConfig.groovy
+++ plugins/griffon-spring/trunk/griffon-app/conf/BuildConfig.groovy
@@ -8,20 +8,26 @@ griffon.project.dependency.resolution = {
 
         mavenCentral()
         mavenRepo 'http://repository.springsource.com/maven/bundles/release'
-        flatDir name: 'springPluginLib', dirs: 'lib'
     }
     dependencies {
         def springVersion = '3.0.5.RELEASE'
-        compile("org.springframework:org.springframework.aop:$springVersion") { transitive = false }
-        compile("org.springframework:org.springframework.asm:$springVersion") { transitive = false }
-        compile("org.springframework:org.springframework.aspects:$springVersion") { transitive = false }
-        compile("org.springframework:org.springframework.core:$springVersion") { transitive = false }
-        compile("org.springframework:org.springframework.beans:$springVersion") { transitive = false }
-        compile("org.springframework:org.springframework.context:$springVersion") { transitive = false }
-        compile("org.springframework:org.springframework.context.support:$springVersion") { transitive = false }
-        compile("org.springframework:org.springframework.expression:$springVersion") { transitive = false }
-        compile("org.springframework:org.springframework.transaction:$springVersion") { transitive = false }
-        compile("org.grails:grails-spring:1.3.6") { transitive = false }
+        compile("org.springframework:org.springframework.aop:$springVersion",
+                "org.springframework:org.springframework.asm:$springVersion",
+                "org.springframework:org.springframework.aspects:$springVersion",
+                "org.springframework:org.springframework.core:$springVersion",
+                "org.springframework:org.springframework.beans:$springVersion",
+                "org.springframework:org.springframework.context:$springVersion",
+                "org.springframework:org.springframework.context.support:$springVersion",
+                "org.springframework:org.springframework.expression:$springVersion",
+                "org.springframework:org.springframework.transaction:$springVersion",
+                "org.springframework:org.springframework.instrument:$springVersion",
+                'cglib:cglib-nodep:2.2',
+                'aopalliance:aopalliance:1.0',
+                'org.aspectj:aspectjweaver:1.6.10',
+                'org.aspectj:aspectjrt:1.6.10',
+                'org.grails:grails-spring:1.3.6') {
+            transitive = false 
+        }
     }
 }
 
