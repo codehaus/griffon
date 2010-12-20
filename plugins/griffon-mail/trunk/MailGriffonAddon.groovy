269083d8022fbd965946bf4d2c7e2c4641eb86d4
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-12-20 02:20:19 +0000

    griffon-mail: upgrade to griffon 0.9.2

--- plugins/griffon-mail/trunk/MailGriffonAddon.groovy
+++ plugins/griffon-mail/trunk/MailGriffonAddon.groovy
@@ -1,18 +1,31 @@
-import griffon.util.IGriffonApplication
+/*
+ * Copyright 2009-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+import griffon.plugins.mail.MailSender
+
+/**
+ * @author Josh A. Reed
+ */
 class MailGriffonAddon {
-	private IGriffonApplication application
-	
-	// add ourselves as a listener so we can respond to events
-	def addonInit = { app ->
-		application = app
-		app.addApplicationEventListener(this)
-	}
-	
-	// inject the 'sendMail' into our 
-	def onNewInstance = { klass, type, instance ->
-		def types = application.config.griffon?.mail?.injectInto ?: ["controller"]
-		if (!types.contains(type)) return
-		instance.metaClass.sendMail = { Map args -> MailService.sendMail(args) }
-	}
+    def events = [
+	    NewInstance: { klass, type, instance ->
+		    def types = app.config.griffon?.mail?.injectInto ?: ['controller']
+	 	    if (!types.contains(type)) return
+            def mc = app.artifactManager.findGriffonClass(klass).metaClass
+		    mc.sendMail = { Map args -> MailSender.sendMail(args) }
+	    }
+    ]
 }
--- plugins/griffon-mail/trunk/src/main/griffon/plugins/mail/MailSender.groovy
+++ plugins/griffon-mail/trunk/src/main/griffon/plugins/mail/MailSender.groovy
@@ -1,22 +1,41 @@
-import groovy.xml.MarkupBuilder;
+/*
+ * Copyright 2009-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import javax.mail.internet.MimeMultipart;
+package griffon.plugins.mail
+
+import groovy.xml.MarkupBuilder
+
+import javax.mail.internet.MimeMultipart
 
 import java.util.Properties
 
 import javax.mail.Message
 import javax.mail.Session
 import javax.mail.internet.InternetAddress
-import javax.activation.DataHandler;
-import javax.activation.FileDataSource;
-import javax.mail.internet.MimeBodyPart;
+import javax.activation.DataHandler
+import javax.activation.FileDataSource
+import javax.mail.internet.MimeBodyPart
 import javax.mail.internet.MimeMessage
 
 /**
  * Sends email from a Griffon application.
+ *
+ * @author Josh A. Reed
  */
-class MailService {
-	
+class MailSender {
 	/**
 	 * Send an email using the arguments specified.  The following arguments are recognized:
 	 *	<ul>
