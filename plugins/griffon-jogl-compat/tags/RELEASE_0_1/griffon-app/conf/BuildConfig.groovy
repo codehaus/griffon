396e45b21026cbb4c93fb3a8f32bcd7c1ec26fc9
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-12-21 11:29:02 +0000

    release

---
+++ plugins/griffon-jogl-compat/tags/RELEASE_0_1/griffon-app/conf/BuildConfig.groovy
@@ -0,0 +1,26 @@
+griffon.project.dependency.resolution = {
+    inherits("global")
+    log "warn"
+    repositories {
+        griffonPlugins()
+        griffonHome()
+        griffonCentral()
+
+        flatDir name: 'joglCompatPluginLib', dirs: 'lib'
+    }
+    dependencies {
+        compile "com.sun:jogl:1.1.1",
+                "com.sun:gluegen-rt:1.0b6"
+    }
+}
+
+griffon {
+    doc {
+        logo = '<a href="http://griffon.codehaus.org" target="_blank"><img alt="The Griffon Framework" src="../img/griffon.png" border="0"/></a>'
+        sponsorLogo = "<br/>"
+        footer = "<br/><br/>Made with Griffon (@griffon.version@)"
+    }
+}
+
+griffon.jars.destDir='target/addon'
+griffon.plugin.pack.additional.sources = ['src/gdsl']
