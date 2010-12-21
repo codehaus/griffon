a4c5679079217b2133989f8b846f683f62ef4cbd
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-12-21 11:59:56 +0000

    release

---
+++ plugins/griffon-trident-builder/tags/LATEST_RELEASE/griffon-app/conf/BuildConfig.groovy
@@ -0,0 +1,25 @@
+griffon.project.dependency.resolution = {
+    inherits("global")
+    log "warn"
+    repositories {
+        griffonPlugins()
+        griffonHome()
+        griffonCentral()
+        flatDir name: 'tridentBuilderPluginLib', dirs: 'lib'
+    }
+    dependencies {
+        compile 'org.codehaus.griffon:tridentbuilder:0.4',
+                'org.pushingpixels:trident:1.3'
+    }
+}
+
+griffon {
+    doc {
+        logo = '<a href="http://griffon.codehaus.org" target="_blank"><img alt="The Griffon Framework" src="../img/griffon.png" border="0"/></a>'
+        sponsorLogo = "<br/>"
+        footer = "<br/><br/>Made with Griffon (@griffon.version@)"
+    }
+}
+
+griffon.jars.destDir='target/addon'
+griffon.plugin.pack.additional.sources = ['src/gdsl']
