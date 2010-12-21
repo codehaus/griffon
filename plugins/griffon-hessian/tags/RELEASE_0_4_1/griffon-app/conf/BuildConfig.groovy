4dfe3a4f9e14fbe0fae0a33d96208f879b20f418
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-12-21 12:09:35 +0000

    release

---
+++ plugins/griffon-hessian/tags/RELEASE_0_4_1/griffon-app/conf/BuildConfig.groovy
@@ -0,0 +1,26 @@
+griffon.project.dependency.resolution = {
+    inherits("global")
+    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    repositories {
+        griffonPlugins()
+        griffonHome()
+        griffonCentral()
+
+        mavenCentral()
+        flatDir name: 'hessianPluginLib', dirs: 'lib'
+    }
+    dependencies {
+        compile 'com.caucho:hessian:4.0.7'
+    }
+}
+
+griffon {
+    doc {
+        logo = '<a href="http://griffon.codehaus.org" target="_blank"><img alt="The Griffon Framework" src="../img/griffon.png" border="0"/></a>'
+        sponsorLogo = "<br/>"
+        footer = "<br/><br/>Made with Griffon (@griffon.version@)"
+    }
+}
+
+griffon.jars.destDir='target/addon'
+griffon.plugin.pack.additional.sources = ['src/gdsl']
