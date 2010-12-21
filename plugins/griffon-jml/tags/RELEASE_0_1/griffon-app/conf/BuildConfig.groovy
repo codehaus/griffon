b4f42e0327e73fd08704e6ce61f499221ba82d5c
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2010-12-21 10:54:39 +0000

    release

---
+++ plugins/griffon-jml/tags/RELEASE_0_1/griffon-app/conf/BuildConfig.groovy
@@ -0,0 +1,27 @@
+griffon.project.dependency.resolution = {
+    inherits("global")
+    log "warn"
+    repositories {
+        griffonPlugins()
+        griffonHome()
+        griffonCentral()
+
+        mavenCentral()
+        flatDir name: 'jmlPluginLib', dirs: 'lib'
+    }
+    dependencies {
+        runtime 'org.apache.httpcomponents:httpcore:4.1',
+                'cindy:cindy:3.0b1',
+                'jml:jml:1.0b4'
+    }
+}
+
+griffon {
+    doc {
+        logo = '<a href="http://griffon.codehaus.org" target="_blank"><img alt="The Griffon Framework" src="../img/griffon.png" border="0"/></a>'
+        sponsorLogo = "<br/>"
+        footer = "<br/><br/>Made with Griffon (@griffon.version@)"
+    }
+}
+
+griffon.jars.destDir='target/addon'
