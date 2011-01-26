ec31de34e0c6a51a2a573f720ebe80e3625040ce
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-01-26 03:19:21 +0000

    griffon-jide-builder: upgrade to 0.9.2

--- plugins/griffon-jide-builder/trunk/JideBuilderGriffonPlugin.groovy
+++ plugins/griffon-jide-builder/trunk/JideBuilderGriffonPlugin.groovy
@@ -18,9 +18,9 @@
  * @author Andres Almiray
  */
 class JideBuilderGriffonPlugin {
-    def version = '0.4'
+    def version = '0.5'
     def dependsOn = [:]
-    def griffonVersion = '0.9 > *'
+    def griffonVersion = '0.9.2 > *'
     def license = 'Apache Software License 2.0'
     def toolkits = ['swing']
 
