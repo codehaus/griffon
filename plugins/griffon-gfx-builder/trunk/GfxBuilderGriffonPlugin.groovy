fbfdf7492c510246fb4c0d8bd44b2dc58743af9d
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-01-26 03:11:36 +0000

    griffon-gfx-builder: upgrade to 0.9.2

--- plugins/griffon-gfx-builder/trunk/GfxBuilderGriffonPlugin.groovy
+++ plugins/griffon-gfx-builder/trunk/GfxBuilderGriffonPlugin.groovy
@@ -18,11 +18,11 @@
  * @author Andres Almiray
  */
 class GfxBuilderGriffonPlugin {
-    def version = '0.4'
+    def version = '0.5'
     def dependsOn = [:]
     def jdk = '1.6'
     def toolkits = ['swing']
-    def griffonVersion = '0.9 > *'
+    def griffonVersion = '0.9.2 > *'
     def license = 'Apache Software License 2.0'
 
     def author = 'Andres Almiray'
--- plugins/griffon-gfx-builder/trunk/griffon-app/conf/BuildConfig.groovy
+++ plugins/griffon-gfx-builder/trunk/griffon-app/conf/BuildConfig.groovy
@@ -1,26 +1,40 @@
 griffon.project.dependency.resolution = {
-    // inherit Griffon' default dependencies
-    inherits("global") {
-    }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    inherits("global")
+    log "warn"
     repositories {
         griffonPlugins()
         griffonHome()
         griffonCentral()
 
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        //mavenLocal()
-        //mavenCentral()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
+        mavenCentral()
+        mavenRepo ' https://repository.jboss.org/nexus/content/groups/public-jboss'
+        flatDir name: 'gfxPluginLib', dirs: 'lib'
     }
     dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-
-        // runtime 'mysql:mysql-connector-java:5.1.5'
+        compile 'org.codehaus.griffon:gfxbuilder-core:0.2.4',
+                'org.codehaus.griffon:gfxbuilder-svg:0.2.4',
+                'org.codehaus.griffon:gfxbuilder-swingx:0.2.4',
+                'org.swinglabs:swingx:1.0',
+                'org.kordamp:jsilhouette-geom:0.3.1',
+                'xml-apis:xml-apis-ext:1.3.04',
+                'batik:batik-anim:1.7',
+                'batik:batik-awt-util:1.7',
+                'batik:batik-bridge:1.7',
+                'batik:batik-codec:1.7',
+                'batik:batik-css:1.7',
+                'batik:batik-dom:1.7',
+                'batik:batik-ext:1.7',
+                'batik:batik-extension:1.7',
+                'batik:batik-gui-util:1.7',
+                'batik:batik-gvt:1.7',
+                'batik:batik-parser:1.7',
+                'batik:batik-script:1.7',
+                'batik:batik-svg-dom:1.7',
+                'batik:batik-svggen:1.7',
+                'batik:batik-swing:1.7',
+                'batik:batik-transcoder:1.7',
+                'batik:batik-util:1.7',
+                'batik:batik-xml:1.7'
     }
 }
 
@@ -28,7 +42,7 @@ griffon {
     doc {
         logo = '<a href="http://griffon.codehaus.org" target="_blank"><img alt="The Griffon Framework" src="../img/griffon.png" border="0"/></a>'
         sponsorLogo = "<br/>"
-        footer = "<br/><br/>Made with Griffon (@griffon.version@)"
+        footer = "<br/><br/>Made with Griffon (0.9)"
     }
 }
 
