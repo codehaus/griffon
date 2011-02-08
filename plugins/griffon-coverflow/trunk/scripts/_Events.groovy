e68cc432556285bca4dbc39cc89581f513f564d8
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-02-08 15:37:29 +0000

    griffon-coverflow: upgrade to griffon 0.9.2

--- plugins/griffon-coverflow/trunk/scripts/_Events.groovy
+++ plugins/griffon-coverflow/trunk/scripts/_Events.groovy
@@ -18,16 +18,15 @@
  * @author Andres Almiray
  */
 
-def eventClosure1 = binding.variables.containsKey('eventCopyLibsEnd') ? eventCopyLibsEnd : {jardir->}
-eventCopyLibsEnd = { jardir ->
-    eventClosure1(jardir)
-    if (!isPluginProject) {
-        def pluginDir = getPluginDirForName('coverflow')
-        if(pluginDir?.file?.exists()) {
-            ant.fileset(dir: "${pluginDir.file}/lib/", includes: '*.jar').each {
-                griffonCopyDist(it.toString(), jardir)
-            }
-        }
-    }
+def eventClosure1 = binding.variables.containsKey('eventSetClasspath') ? eventSetClasspath : {cl->}
+eventSetClasspath = { cl ->
+    eventClosure1(cl)
+    if(compilingPlugin('coverflow')) return
+    griffonSettings.dependencyManager.flatDirResolver name: 'griffon-coverflow-plugin', dirs: "${coverflowPluginDir}/addon"
+    griffonSettings.dependencyManager.addPluginDependency('coverflow', [
+        conf: 'compile',
+        name: 'griffon-coverflow-addon',
+        group: 'org.codehaus.griffon.plugins',
+        version: coverflowPluginVersion
+    ])
 }
-
