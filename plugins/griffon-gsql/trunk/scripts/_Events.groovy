cc1d6ed39dd28d5696af79e3bf6050c2b1f3b409
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-02-08 15:43:20 +0000

    griffon-gsql: upgrade to griffon 0.9.2

--- plugins/griffon-gsql/trunk/scripts/_Events.groovy
+++ plugins/griffon-gsql/trunk/scripts/_Events.groovy
@@ -18,16 +18,16 @@
  * @author Andres Almiray
  */
 
-def eventClosure1 = binding.variables.containsKey('eventCopyLibsEnd') ? eventCopyLibsEnd : {jardir->}
-eventCopyLibsEnd = { jardir ->
-    eventClosure1(jardir)
-    if (!isPluginProject) {
-        def pluginDir = getPluginDirForName('gsql')
-        if(pluginDir?.file?.exists()) {
-            ant.fileset(dir: "${pluginDir.file}/lib/", includes: '*.jar').each {
-                griffonCopyDist(it.toString(), jardir)
-            }
-        }
-    }
+def eventClosure1 = binding.variables.containsKey('eventSetClasspath') ? eventSetClasspath : {cl->}
+eventSetClasspath = { cl ->
+    eventClosure1(cl)
+    if(compilingPlugin('gsql')) return
+    griffonSettings.dependencyManager.flatDirResolver name: 'griffon-gsql-plugin', dirs: "${gsqlPluginDir}/addon"
+    griffonSettings.dependencyManager.addPluginDependency('gsql', [
+        conf: 'compile',
+        name: 'griffon-gsql-addon',
+        group: 'org.codehaus.griffon.plugins',
+        version: gsqlPluginVersion
+    ])
 }
 
