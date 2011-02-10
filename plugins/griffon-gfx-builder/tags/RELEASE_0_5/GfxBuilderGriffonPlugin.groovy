e05d8576556de632419dc6a898e96d1a48844ac7
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-02-10 12:38:25 +0000

    griffon-gfx-builder:

---
+++ plugins/griffon-gfx-builder/tags/RELEASE_0_5/GfxBuilderGriffonPlugin.groovy
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2009-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author Andres Almiray
+ */
+class GfxBuilderGriffonPlugin {
+    def version = '0.5'
+    def dependsOn = [:]
+    def jdk = '1.6'
+    def toolkits = ['swing']
+    def griffonVersion = '0.9.2 > *'
+    def license = 'Apache Software License 2.0'
+
+    def author = 'Andres Almiray'
+    def authorEmail = 'aalmiray@users.sourceforge.net'
+    def title = 'Gfx Builder Plugin'
+    def description = '''
+Brings de GfxBuilder libraries into a Griffon application
+'''
+
+    // URL to the plugin's documentation
+    def documentation = 'http://griffon.codehaus.org/GfxBuilder+Plugin'
+}
---
+++ plugins/griffon-gfx-builder/tags/RELEASE_0_5/griffon-app/conf/BuildConfig.groovy
@@ -0,0 +1,50 @@
+griffon.project.dependency.resolution = {
+    inherits("global")
+    log "warn"
+    repositories {
+        griffonPlugins()
+        griffonHome()
+        griffonCentral()
+
+        mavenCentral()
+        mavenRepo ' https://repository.jboss.org/nexus/content/groups/public-jboss'
+        flatDir name: 'gfxPluginLib', dirs: 'lib'
+    }
+    dependencies {
+        compile 'org.codehaus.griffon:gfxbuilder-core:0.2.4',
+                'org.codehaus.griffon:gfxbuilder-svg:0.2.4',
+                'org.codehaus.griffon:gfxbuilder-swingx:0.2.4',
+                'org.swinglabs:swingx:1.0',
+                'org.kordamp:jsilhouette-geom:0.3.1',
+                'xml-apis:xml-apis-ext:1.3.04',
+                'batik:batik-anim:1.7',
+                'batik:batik-awt-util:1.7',
+                'batik:batik-bridge:1.7',
+                'batik:batik-codec:1.7',
+                'batik:batik-css:1.7',
+                'batik:batik-dom:1.7',
+                'batik:batik-ext:1.7',
+                'batik:batik-extension:1.7',
+                'batik:batik-gui-util:1.7',
+                'batik:batik-gvt:1.7',
+                'batik:batik-parser:1.7',
+                'batik:batik-script:1.7',
+                'batik:batik-svg-dom:1.7',
+                'batik:batik-svggen:1.7',
+                'batik:batik-swing:1.7',
+                'batik:batik-transcoder:1.7',
+                'batik:batik-util:1.7',
+                'batik:batik-xml:1.7'
+    }
+}
+
+griffon {
+    doc {
+        logo = '<a href="http://griffon.codehaus.org" target="_blank"><img alt="The Griffon Framework" src="../img/griffon.png" border="0"/></a>'
+        sponsorLogo = "<br/>"
+        footer = "<br/><br/>Made with Griffon (0.9)"
+    }
+}
+
+griffon.jars.destDir='target/addon'
+griffon.plugin.pack.additional.sources = ['src/gdsl']
