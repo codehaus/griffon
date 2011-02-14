1166e4a92dd5c032893427ca5dfa783c65772c3f
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-02-14 08:50:02 +0000

    griffon-designgridlayout:

---
+++ plugins/griffon-designgridlayout/tags/LATEST_RELEASE/griffon-app/conf/BuildConfig.groovy
@@ -0,0 +1,24 @@
+griffon.project.dependency.resolution = {
+    inherits "global"
+    log "warn"
+    repositories {
+        griffonPlugins()
+        griffonHome()
+        griffonCentral()
+        mavenRepo 'http://repository.sonatype.org/content/groups/public'
+    }
+    dependencies {
+        compile('net.java.dev.designgridlayout:designgridlayout:1.7')
+    }
+}
+
+griffon {
+    doc {
+        logo = '<a href="http://griffon.codehaus.org" target="_blank"><img alt="The Griffon Framework" src="../img/griffon.png" border="0"/></a>'
+        sponsorLogo = "<br/>"
+        footer = "<br/><br/>Made with Griffon (@griffon.version@)"
+    }
+}
+
+griffon.jars.destDir='target/addon'
+griffon.plugin.pack.additional.sources = ['src/gdsl']
