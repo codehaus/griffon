8a2b1e15d28ec6940d13a5d422785307e56bac4a
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-02-14 08:58:00 +0000

    griffon-jgoodies-forms:

--- plugins/griffon-jgoodies-forms/trunk/scripts/_Events.groovy
+++ plugins/griffon-jgoodies-forms/trunk/scripts/_Events.groovy
@@ -18,15 +18,16 @@
  * @author Andres Almiray
  */
 
-def eventClosure1 = binding.variables.containsKey('eventCopyLibsEnd') ? eventCopyLibsEnd : {jardir->}
-eventCopyLibsEnd = { jardir ->
-    eventClosure1(jardir)
-    if (!isPluginProject) {
-        def pluginDir = getPluginDirForName('jgoodies-forms')
-        if(pluginDir?.file?.exists()) {
-            ant.fileset(dir: "${pluginDir.file}/lib/", includes: '*.jar').each {
-                griffonCopyDist(it.toString(), jardir)
-            }
-        }
-    }
+def eventClosure1 = binding.variables.containsKey('eventSetClasspath') ? eventSetClasspath : {cl->}
+eventSetClasspath = { cl ->
+    eventClosure1(cl)
+    if(compilingPlugin('jgoodies-forms')) return
+    griffonSettings.dependencyManager.flatDirResolver name: 'griffon-jgoodies-forms-plugin', dirs: "${jgoodiesFormsPluginDir}/addon"
+    griffonSettings.dependencyManager.addPluginDependency('jgoodies-forms', [
+        conf: 'compile',
+        name: 'griffon-jgoodies-forms-addon',
+        group: 'org.codehaus.griffon.plugins',
+        version: jgoodiesFormsPluginVersion
+    ])
 }
+
