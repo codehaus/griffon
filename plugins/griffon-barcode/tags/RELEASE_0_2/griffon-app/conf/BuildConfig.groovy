baaf91e7ed4ed3ce95a402b029f32a35b454c027
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-02-21 16:01:03 +0000

    griffon-barcode:

---
+++ plugins/griffon-barcode/tags/RELEASE_0_2/griffon-app/conf/BuildConfig.groovy
@@ -0,0 +1,24 @@
+griffon.project.dependency.resolution = {
+    inherits("global")
+    log "warn"
+    repositories {
+        griffonPlugins()
+        griffonHome()
+        griffonCentral()
+
+        mavenCentral()
+    }
+    dependencies {
+        compile('net.sf.barcode4j:barcode4j:2.0') { transitive = false }
+    }
+}
+
+griffon {
+    doc {
+        logo = '<a href="http://griffon.codehaus.org" target="_blank"><img alt="The Griffon Framework" src="../img/griffon.png" border="0"/></a>'
+        sponsorLogo = "<br/>"
+        footer = "<br/><br/>Made with Griffon (@griffon.version@)"
+    }
+}
+
+griffon.jars.destDir='target/addon'
