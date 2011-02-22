c7cd880207cd245c5b633988fe83c00fe1397b7b
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-02-22 20:24:59 +0000

    release

---
+++ plugins/griffon-lookandfeel-jtattoo/tags/RELEASE_0_3/griffon-app/conf/BuildConfig.groovy
@@ -0,0 +1,23 @@
+griffon.project.dependency.resolution = {
+    inherits("global")
+    log "warn"
+    repositories {
+        griffonPlugins()
+        griffonHome()
+        griffonCentral()
+        flatDir name: 'lookandfeelJtattooPluginLib', dirs: 'lib'
+    }
+    dependencies {
+        runtime 'com.jtattoo:jtattoo:1.4.1'
+    }
+}
+
+griffon {
+    doc {
+        logo = '<a href="http://griffon.codehaus.org" target="_blank"><img alt="The Griffon Framework" src="../img/griffon.png" border="0"/></a>'
+        sponsorLogo = "<br/>"
+        footer = "<br/><br/>Made with Griffon (@griffon.version@)"
+    }
+}
+
+griffon.jars.destDir='target/addon'
