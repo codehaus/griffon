978f9c67435faf0c7867d0dc187843fe8837c6b4
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-02-22 20:13:44 +0000

    release

---
+++ plugins/griffon-lookandfeel/tags/RELEASE_0_4/LookandfeelGriffonPlugin.groovy
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author Andres Almiray
+ */
+class LookandfeelGriffonPlugin {
+    def version = 0.4
+    def griffonVersion = '0.9.2 > *' 
+    def dependsOn = [miglayout: 0.2]
+    def toolkits = ['swing']
+    def license = 'Apache Software License 2.0'
+
+    def author = 'Andres Almiray'
+    def authorEmail = 'aalmiray@users.sourceforge.net'
+    def title = 'Swing Look & Feel selector'
+    def description = '''
+Swing Look & Feel selector.
+'''
+
+    // URL to the plugin's documentation
+    def documentation = 'http://griffon.codehaus.org/Lookandfeel+Plugin'
+}
