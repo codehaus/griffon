86dae3fafa481365cfb1eb29791fa557d1c8aa8d
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-02-22 11:50:16 +0000

    upgraded

--- plugins/griffon-jbusycomponent/trunk/JbusycomponentGriffonPlugin.groovy
+++ plugins/griffon-jbusycomponent/trunk/JbusycomponentGriffonPlugin.groovy
@@ -18,8 +18,8 @@
  * @author Andres Almiray
  */
 class JbusycomponentGriffonPlugin {
-    def version = '0.5'
-    def dependsOn = ['swingx-builder': '0.3', jxlayer: 0.2]
+    def version = '0.5.1'
+    def dependsOn = ['swingx-builder': '0.4', jxlayer: 0.2]
     def toolkits = ['swing']
     def griffonVersion = '0.9.2 > *'
     def license = 'Apache Software License 2.0'
