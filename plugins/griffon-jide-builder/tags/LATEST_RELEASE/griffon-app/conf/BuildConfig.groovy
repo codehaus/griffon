96062992cbba97eb0b693778cfc8aba7350962ea
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-02-22 11:41:47 +0000

    release

---
+++ plugins/griffon-jide-builder/tags/LATEST_RELEASE/griffon-app/conf/BuildConfig.groovy
@@ -0,0 +1,29 @@
+griffon.project.dependency.resolution = {
+    inherits("global")
+    log "warn"
+    repositories {
+        griffonPlugins()
+        griffonHome()
+        griffonCentral()
+        flatDir name: 'jideBuilderPluginLib', dirs: 'lib'
+        mavenCentral()
+        mavenRepo 'http://repository.codehaus.org'
+        mavenRepo 'http://repository.sonatype.org/content/groups/public'
+    }
+    dependencies {
+        compile('org.codehaus.griffon:jidebuilder:3.0') {
+            excludes 'groovy-all', 'svg-salamander'
+        }
+        compile 'com.kitfox.svg:svg-salamander:1.0'
+    }
+}
+
+griffon {
+    doc {
+        logo = '<a href="http://griffon.codehaus.org" target="_blank"><img alt="The Griffon Framework" src="../img/griffon.png" border="0"/></a>'
+        sponsorLogo = "<br/>"
+        footer = "<br/><br/>Made with Griffon (@griffon.version@)"
+    }
+}
+
+griffon.jars.destDir='target/addon'
