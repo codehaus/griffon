12987d9cf579654b223ec9c3dc040339d2d44c68
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-02-22 20:24:37 +0000

    release

--- plugins/griffon-lookandfeel-jtattoo/trunk/scripts/_Events.groovy
+++ plugins/griffon-lookandfeel-jtattoo/trunk/scripts/_Events.groovy
@@ -18,16 +18,15 @@
  * @author Andres Almiray
  */
 
-def eventClosure1 = binding.variables.containsKey('eventCopyLibsEnd') ? eventCopyLibsEnd : {jardir->}
-eventCopyLibsEnd = { jardir ->
-    eventClosure1(jardir)
-    if (!isPluginProject) {
-        def pluginDir = getPluginDirForName('lookandfeel-jtattoo')
-        if(pluginDir?.file?.exists()) {
-            ant.fileset(dir: "${pluginDir.file}/lib/", includes: '*.jar').each {
-                griffonCopyDist(it.toString(), jardir)
-            }
-        }
-    }
+def eventClosure1 = binding.variables.containsKey('eventSetClasspath') ? eventSetClasspath : {cl->}
+eventSetClasspath = { cl ->
+    eventClosure1(cl)
+    if(compilingPlugin('lookandfeel-jtattoo')) return
+    griffonSettings.dependencyManager.flatDirResolver name: 'griffon-lookandfeelJtattoo-plugin', dirs: "${lookandfeelJtattooPluginDir}/addon"
+    griffonSettings.dependencyManager.addPluginDependency('lookandfeelJtattoo', [
+        conf: 'compile',
+        name: 'griffon-lookandfeel-jtattoo-addon',
+        group: 'org.codehaus.griffon.plugins',
+        version: lookandfeelJtattooPluginVersion
+    ])
 }
-
