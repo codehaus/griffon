7ad81abd4d5de2afe2d6f6a6c65b60ec324051db
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-02-22 20:18:59 +0000

    release

--- plugins/griffon-lookandfeel-a03/trunk/scripts/_Events.groovy
+++ plugins/griffon-lookandfeel-a03/trunk/scripts/_Events.groovy
@@ -18,16 +18,15 @@
  * @author Andres Almiray
  */
 
-def eventClosure1 = binding.variables.containsKey('eventCopyLibsEnd') ? eventCopyLibsEnd : {jardir->}
-eventCopyLibsEnd = { jardir ->
-    eventClosure1(jardir)
-    if (!isPluginProject) {
-        def pluginDir = getPluginDirForName('lookandfeel-a03')
-        if(pluginDir?.file?.exists()) {
-            ant.fileset(dir: "${pluginDir.file}/lib/", includes: '*.jar').each {
-                griffonCopyDist(it.toString(), jardir)
-            }
-        }
-    }
+def eventClosure1 = binding.variables.containsKey('eventSetClasspath') ? eventSetClasspath : {cl->}
+eventSetClasspath = { cl ->
+    eventClosure1(cl)
+    if(compilingPlugin('lookandfeel-a03')) return
+    griffonSettings.dependencyManager.flatDirResolver name: 'griffon-lookandfeelA03-plugin', dirs: "${lookandfeelA03PluginDir}/addon"
+    griffonSettings.dependencyManager.addPluginDependency('lookandfeelA03', [
+        conf: 'compile',
+        name: 'griffon-lookandfeel-a03-addon',
+        group: 'org.codehaus.griffon.plugins',
+        version: lookandfeelA03PluginVersion
+    ])
 }
-
