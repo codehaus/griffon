a313ef92a35ab70f26978b582522410f5b59f35f
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-02-24 16:43:36 +0000

    release

--- plugins/griffon-lookandfeel-substance/trunk/LookandfeelSubstanceGriffonPlugin.groovy
+++ plugins/griffon-lookandfeel-substance/trunk/LookandfeelSubstanceGriffonPlugin.groovy
@@ -18,10 +18,10 @@
  * @author Andres Almiray
  */
 class LookandfeelSubstanceGriffonPlugin {
-    def version = 0.2
-    def griffonVersion = '0.9 > *'
-    def dependsOn = [lookandfeel: 0.2,
-                     'trident-builder': 0.5]
+    def version = 0.3
+    def griffonVersion = '0.9.2 > *'
+    def dependsOn = [lookandfeel: 0.4,
+                     'trident-builder': '0.6.1']
     def toolkits = ['swing']
     def license = 'Apache Software License 2.0'
 
--- plugins/griffon-lookandfeel-substance/trunk/scripts/_Events.groovy
+++ plugins/griffon-lookandfeel-substance/trunk/scripts/_Events.groovy
@@ -18,16 +18,15 @@
  * @author Andres Almiray
  */
 
-def eventClosure1 = binding.variables.containsKey('eventCopyLibsEnd') ? eventCopyLibsEnd : {jardir->}
-eventCopyLibsEnd = { jardir ->
-    eventClosure1(jardir)
-    if (!isPluginProject) {
-        def pluginDir = getPluginDirForName('lookandfeel-substance')
-        if(pluginDir?.file?.exists()) {
-            ant.fileset(dir: "${pluginDir.file}/lib/", includes: '*.jar').each {
-                griffonCopyDist(it.toString(), jardir)
-            }
-        }
-    }
+def eventClosure1 = binding.variables.containsKey('eventSetClasspath') ? eventSetClasspath : {cl->}
+eventSetClasspath = { cl ->
+    eventClosure1(cl)
+    if(compilingPlugin('lookandfeel-substance')) return
+    griffonSettings.dependencyManager.flatDirResolver name: 'griffon-lookandfeelSubstance-plugin', dirs: "${lookandfeelSubstancePluginDir}/addon"
+    griffonSettings.dependencyManager.addPluginDependency('lookandfeelSubstance', [
+        conf: 'compile',
+        name: 'griffon-lookandfeel-substance-addon',
+        group: 'org.codehaus.griffon.plugins',
+        version: lookandfeelSubstancePluginVersion
+    ])
 }
-
