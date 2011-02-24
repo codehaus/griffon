ac57ba8b8bd42c7fb50d7eb42667fa7af34466e6
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-02-24 13:00:02 +0000

    release

--- plugins/griffon-lookandfeel-nimrod/trunk/scripts/_Events.groovy
+++ plugins/griffon-lookandfeel-nimrod/trunk/scripts/_Events.groovy
@@ -18,16 +18,15 @@
  * @author Andres Almiray
  */
 
-def eventClosure1 = binding.variables.containsKey('eventCopyLibsEnd') ? eventCopyLibsEnd : {jardir->}
-eventCopyLibsEnd = { jardir ->
-    eventClosure1(jardir)
-    if (!isPluginProject) {
-        def pluginDir = getPluginDirForName('lookandfeel-nimrod')
-        if(pluginDir?.file?.exists()) {
-            ant.fileset(dir: "${pluginDir.file}/lib/", includes: '*.jar').each {
-                griffonCopyDist(it.toString(), jardir)
-            }
-        }
-    }
+def eventClosure1 = binding.variables.containsKey('eventSetClasspath') ? eventSetClasspath : {cl->}
+eventSetClasspath = { cl ->
+    eventClosure1(cl)
+    if(compilingPlugin('lookandfeel-nimrod')) return
+    griffonSettings.dependencyManager.flatDirResolver name: 'griffon-lookandfeelNimrod-plugin', dirs: "${lookandfeelNimrodPluginDir}/addon"
+    griffonSettings.dependencyManager.addPluginDependency('lookandfeelNimrod', [
+        conf: 'compile',
+        name: 'griffon-lookandfeel-nimrod-addon',
+        group: 'org.codehaus.griffon.plugins',
+        version: lookandfeelNimrodPluginVersion
+    ])
 }
-
