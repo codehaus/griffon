5bd9b2c3f6976b173718f0244d91afc0725336b2
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-02-24 12:56:34 +0000

    release

--- plugins/griffon-lookandfeel-metouia/trunk/scripts/_Events.groovy
+++ plugins/griffon-lookandfeel-metouia/trunk/scripts/_Events.groovy
@@ -18,16 +18,15 @@
  * @author Andres Almiray
  */
 
-def eventClosure1 = binding.variables.containsKey('eventCopyLibsEnd') ? eventCopyLibsEnd : {jardir->}
-eventCopyLibsEnd = { jardir ->
-    eventClosure1(jardir)
-    if (!isPluginProject) {
-        def pluginDir = getPluginDirForName('lookandfeel-metouia')
-        if(pluginDir?.file?.exists()) {
-            ant.fileset(dir: "${pluginDir.file}/lib/", includes: '*.jar').each {
-                griffonCopyDist(it.toString(), jardir)
-            }
-        }
-    }
+def eventClosure1 = binding.variables.containsKey('eventSetClasspath') ? eventSetClasspath : {cl->}
+eventSetClasspath = { cl ->
+    eventClosure1(cl)
+    if(compilingPlugin('lookandfeel-metouia')) return
+    griffonSettings.dependencyManager.flatDirResolver name: 'griffon-lookandfeelMetouia-plugin', dirs: "${lookandfeelMetouiaPluginDir}/addon"
+    griffonSettings.dependencyManager.addPluginDependency('lookandfeelMetouia', [
+        conf: 'compile',
+        name: 'griffon-lookandfeel-metouia-addon',
+        group: 'org.codehaus.griffon.plugins',
+        version: lookandfeelMetouiaPluginVersion
+    ])
 }
-
