e9a00c164150145dcf89cfca69fda932ab2ec9d9
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-02-24 17:45:13 +0000

    release

---
+++ plugins/griffon-lookandfeel-squareness/tags/LATEST_RELEASE/src/main/griffon/lookandfeel/squareness/SquarenessLookAndFeelProvider.groovy
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package griffon.lookandfeel.squareness
+
+import java.awt.Component
+import javax.swing.LookAndFeel
+import griffon.lookandfeel.DefaultLookAndFeelProvider
+import griffon.lookandfeel.DefaultLookAndFeelInfo
+import net.beeger.squareness.SquarenessLookAndFeel
+
+/**
+ * @author Andres Almiray
+ */
+class SquarenessLookAndFeelProvider extends DefaultLookAndFeelProvider {
+    private static final List<SquarenessLookAndFeelInfo> SUPPORTED_LAFS = [
+        new SquarenessLookAndFeelInfo()
+    ]
+
+    SquarenessLookAndFeelProvider() {
+        super('Squareness')
+    }
+    
+    boolean handles(LookAndFeel lookAndFeel) {
+        lookAndFeel?.class.name == SquarenessLookAndFeel.class.name
+    }
+
+    boolean handles(griffon.lookandfeel.LookAndFeelInfo lookAndFeelInfo) {
+        lookAndFeelInfo instanceof SquarenessLookAndFeelInfo
+    }
+
+    griffon.lookandfeel.LookAndFeelInfo[] getSupportedLookAndFeels() {
+        return SUPPORTED_LAFS as griffon.lookandfeel.LookAndFeelInfo[]
+    }
+
+    /**
+     * @author Andres Almiray
+     */
+    private static class SquarenessLookAndFeelInfo extends DefaultLookAndFeelInfo {
+        SquarenessLookAndFeelInfo() {
+            super('squareness-squareness', 'Squareness', new SquarenessLookAndFeel())
+        }
+    }
+}
