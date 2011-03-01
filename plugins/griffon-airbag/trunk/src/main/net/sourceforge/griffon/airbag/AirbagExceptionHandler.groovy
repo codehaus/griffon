012b18651363d7b24ede5b543f132c266f3b4b4e
aucifer_nick <aucifer_nick@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-03-01 19:56:03 +0000

    created addon hook to replace default exception handler

---
+++ plugins/griffon-airbag/trunk/src/main/net/sourceforge/griffon/airbag/AirbagExceptionHandler.groovy
@@ -0,0 +1,17 @@
+package net.sourceforge.griffon.airbag
+
+import griffon.util.GriffonExceptionHandler
+
+/**
+ * @author Nick Zhu (nzhu@jointsource.com)
+ */
+class AirbagExceptionHandler extends GriffonExceptionHandler {
+
+    @Override
+    void uncaughtException(Thread t, Throwable e) {
+        super.uncaughtException(t, e)
+
+        println "AirbagExceptionHandler -> ${e.message}"
+    }
+
+}
