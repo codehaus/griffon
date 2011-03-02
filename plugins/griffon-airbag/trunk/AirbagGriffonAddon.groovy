a6283abf5366d89385b900389d5d1fe603ed6e7b
aucifer_nick <aucifer_nick@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-03-02 23:07:28 +0000

    trying to override the default edt error handler setting in griffon

--- plugins/griffon-airbag/trunk/AirbagGriffonAddon.groovy
+++ plugins/griffon-airbag/trunk/AirbagGriffonAddon.groovy
@@ -1,9 +1,18 @@
-import griffon.core.GriffonApplication
 import net.sourceforge.griffon.airbag.AirbagExceptionHandler
+import javax.swing.SwingUtilities
 
 class AirbagGriffonAddon {
-    def addonInit(GriffonApplication app) {
-        Thread.setDefaultUncaughtExceptionHandler(new AirbagExceptionHandler())
-        System.setProperty("sun.awt.exception.handler", AirbagExceptionHandler.class.getName())
-    }
+    def events = [
+            "ReadyEnd": {app ->
+                def exceptionHandler = new AirbagExceptionHandler()
+
+                Thread.setDefaultUncaughtExceptionHandler(exceptionHandler)
+
+                System.setProperty("sun.awt.exception.handler", AirbagExceptionHandler.class.getName())
+            },
+
+            "LogC4jonfigStart": {app ->
+                System.setProperty("sun.awt.exception.handler", AirbagExceptionHandler.class.getName())
+            }
+    ]
 }
