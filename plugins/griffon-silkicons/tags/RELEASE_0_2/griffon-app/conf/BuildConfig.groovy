58de04d9424b35e8f310a5a367933a96bb76d03f
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-03-09 11:19:52 +0000

    release

---
+++ plugins/griffon-silkicons/tags/RELEASE_0_2/griffon-app/conf/BuildConfig.groovy
@@ -0,0 +1,25 @@
+griffon.project.dependency.resolution = {
+    inherits "global"
+    log "warn"
+    repositories {
+        griffonPlugins()
+        griffonHome()
+        griffonCentral()
+        mavenRepo 'http://repository.sonatype.org/content/groups/public'
+    }
+    dependencies {
+        build   'com.famfamfam:silk:1.3'
+        runtime 'com.famfamfam:silk:1.3'
+    }
+}
+
+griffon {
+    doc {
+        logo = '<a href="http://griffon.codehaus.org" target="_blank"><img alt="The Griffon Framework" src="../img/griffon.png" border="0"/></a>'
+        sponsorLogo = "<br/>"
+        footer = "<br/><br/>Made with Griffon (@griffon.version@)"
+    }
+}
+
+griffon.jars.destDir='target/addon'
+griffon.plugin.pack.additional.sources = ['src/gdsl']
