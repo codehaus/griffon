527e3a29e6d4336e79a63780728d3eaf1e0405f7
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-03-09 11:17:48 +0000

    release

--- plugins/griffon-nuvolaicons/trunk/scripts/_Events.groovy
+++ plugins/griffon-nuvolaicons/trunk/scripts/_Events.groovy
@@ -18,15 +18,15 @@
  * @author Andres Almiray
  */
 
-def eventClosure1 = binding.variables.containsKey('eventCopyLibsEnd') ? eventCopyLibsEnd : {jardir->}
-eventCopyLibsEnd = { jardir ->
-    eventClosure1(jardir)
-    if (!isPluginProject) {
-        def pluginDir = getPluginDirForName('nuvolaicons')
-        if(pluginDir?.file?.exists()) {
-            ant.fileset(dir: "${pluginDir.file}/lib/", includes: '*.jar').each {
-                griffonCopyDist(it.toString(), jardir)
-            }
-        }
-    }
+def eventClosure1 = binding.variables.containsKey('eventSetClasspath') ? eventSetClasspath : {cl->}
+eventSetClasspath = { cl ->
+    eventClosure1(cl)
+    if(compilingPlugin('nuvolaicons')) return
+    griffonSettings.dependencyManager.flatDirResolver name: 'griffon-nuvolaicons-plugin', dirs: "${nuvolaiconsPluginDir}/addon"
+    griffonSettings.dependencyManager.addPluginDependency('nuvolaicons', [
+        conf: 'compile',
+        name: 'griffon-nuvolaicons-addon',
+        group: 'org.codehaus.griffon.plugins',
+        version: nuvolaiconsPluginVersion
+    ])
 }
