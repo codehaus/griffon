303c664b8d2205c9dfa10cd32701710bd790f8a4
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-03-09 11:21:26 +0000

    release

---
+++ plugins/griffon-feeds/tags/RELEASE_0_2/src/main/griffon/feeds/FeedIconFactory.groovy
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package griffon.feeds
+
+import groovy.swing.factory.ImageIconFactory
+
+/**
+ * @author Andres.Almiray
+ */
+class FeedIconFactory extends ImageIconFactory {
+    public Object newInstance(FactoryBuilderSupport builder, Object name, Object value, Map attributes) {
+        String size = attributes.remove('size') ?: '16'
+        String icon = attributes.remove('icon') ?: value 
+  
+        if(!icon) throw new IllegalArgumentException("In $name you must define a node value or icon:")
+
+        size = parseSize(size)
+        if(icon.endsWith('.png')) icon -= '.png'
+
+        value = "/com/zeusboxstudio/icons/${size}/${icon}.png"
+        super.newInstance(builder, name, value, [:])
+    }
+
+    private String parseSize(String size) {
+        switch(size) {
+            case '16':
+            case '32':
+                return size +"x"+ size
+        }
+        return '16x16'
+    }
+}
