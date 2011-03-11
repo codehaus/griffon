3dec203c22de7beaca44520e001b98a80996d761
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-03-11 17:41:13 +0000

    release

--- plugins/griffon-jcalendar/trunk/scripts/_Events.groovy
+++ plugins/griffon-jcalendar/trunk/scripts/_Events.groovy
@@ -18,15 +18,16 @@
  * @author Andres Almiray
  */
 
-def eventClosure1 = binding.variables.containsKey('eventCopyLibsEnd') ? eventCopyLibsEnd : {jardir->}
-eventCopyLibsEnd = { jardir ->
-    eventClosure1(jardir)
-    if (!isPluginProject) {
-        def pluginDir = getPluginDirForName('jcalendar')
-        if(pluginDir?.file?.exists()) {
-            ant.fileset(dir: "${pluginDir.file}/lib/", includes: '*.jar').each {
-                griffonCopyDist(it.toString(), jardir)
-            }
-        }
-    }
+def eventClosure1 = binding.variables.containsKey('eventSetClasspath') ? eventSetClasspath : {cl->}
+eventSetClasspath = { cl ->
+    eventClosure1(cl)
+    if(compilingPlugin('jcalendar')) return
+    griffonSettings.dependencyManager.flatDirResolver name: 'griffon-jcalendar-plugin', dirs: "${jcalendarPluginDir}/addon"
+    griffonSettings.dependencyManager.addPluginDependency('jcalendar', [
+        conf: 'compile',
+        name: 'griffon-jcalendar-addon',
+        group: 'org.codehaus.griffon.plugins',
+        version: jcalendarPluginVersion
+    ])
 }
+
