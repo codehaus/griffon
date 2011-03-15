bf6bf342e4213b3293122230d29e9899e508d287
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-03-15 09:41:27 +0000

    quick

---
+++ plugins/griffon-glazedlists/tags/LATEST_RELEASE/griffon-app/conf/BuildConfig.groovy
@@ -0,0 +1,23 @@
+griffon.project.dependency.resolution = {
+    inherits("global")
+    log "warn" 
+    repositories {
+        griffonPlugins()
+        griffonHome()
+        griffonCentral()
+        flatDir name: 'glazedlistsPluginLib', dirs: 'lib'
+    }
+    dependencies {
+        compile 'ca.odell:glazedlists:1.8.0_java15'
+    }
+}
+
+griffon {
+    doc {
+        logo = '<a href="http://griffon.codehaus.org" target="_blank"><img alt="The Griffon Framework" src="../img/griffon.png" border="0"/></a>'
+        sponsorLogo = "<br/>"
+        footer = "<br/><br/>Made with Griffon (@griffon.version@)"
+    }
+}
+
+griffon.jars.destDir='target/addon'
