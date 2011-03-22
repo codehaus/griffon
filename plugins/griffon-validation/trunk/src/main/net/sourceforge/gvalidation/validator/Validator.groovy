becd756741f56cdb9545f54b7e6bc81cc387c1b7
aucifer_nick <aucifer_nick@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-03-22 19:01:27 +0000

    introduced validator interface and refactored blank constraint

--- plugins/griffon-validation/trunk/src/main/net/sourceforge/gvalidation/validator/Validator.groovy
+++ plugins/griffon-validation/trunk/src/main/net/sourceforge/gvalidation/validator/Validator.groovy
@@ -1,17 +1,21 @@
-import net.sourceforge.gvalidation.validator.Validator
+package net.sourceforge.gvalidation.validator
 
-@artifact.package@class @artifact.name@ implements Validator {
+/**
+ * Generic base interface for all validator implementation
+ *
+ * @author Nick Zhu (nzhu@jointsource.com)
+ */
+public interface Validator {
 
     /**
-     * Generated message
+     * Validate method
      *
      * @param propertyValue value of the property to be validated
      * @param bean object owner of the property
      * @param parameter configuration parameter for the constraint
      * @return return true if validation passes otherwise false
      */
-    def validate(propertyValue, bean, parameter) {
-        // insert your custom constraint logic
-    }
+    def validate(propertyValue, bean, parameter)
+
 
 }
