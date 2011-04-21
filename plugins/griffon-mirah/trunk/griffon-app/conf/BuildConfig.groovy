79f180d6cdb45648e3f839816e7d0a6df8b13a82
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-04-21 07:15:11 +0000

    essage

---
+++ plugins/griffon-mirah/trunk/griffon-app/conf/BuildConfig.groovy
@@ -0,0 +1,21 @@
+griffon.project.dependency.resolution = {
+    inherits "global"
+    log "warn" 
+    repositories {
+        griffonPlugins()
+        griffonHome()
+        griffonCentral()
+        mavenCentral()
+    }
+    dependencies {
+        build('org.mirah:mirah-complete:0.0.7')
+    }
+}
+
+griffon {
+    doc {
+        logo = '<a href="http://griffon.codehaus.org" target="_blank"><img alt="The Griffon Framework" src="../img/griffon.png" border="0"/></a>'
+        sponsorLogo = "<br/>"
+        footer = "<br/><br/>Made with Griffon (@griffon.version@)"
+    }
+}
