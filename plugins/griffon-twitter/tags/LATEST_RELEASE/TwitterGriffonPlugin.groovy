d082ba9c52be56b1eb3f303d4674d5835b1dec37
marioggar <marioggar@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-05-01 00:14:10 +0000

    Updating after a failure upload

---
+++ plugins/griffon-twitter/tags/LATEST_RELEASE/TwitterGriffonPlugin.groovy
@@ -0,0 +1,18 @@
+
+class TwitterGriffonPlugin {
+
+	def version = "0.1"
+    def griffonVersion = '0.9.2-rc1 > *' 
+    def dependsOn = [:]
+    def pluginIncludes = []
+    def license = 'Apache 2.0'    
+    def toolkits = [] // Valid values are: swing, javafx, swt, pivot, gtk
+    def platforms = []    // linux, linux64, windows, windows64, macosx, macosx64, solaris
+    def author = 'Mario García'
+    def authorEmail = ''
+    def title = 'Twitter Plugin'
+    def description = '''
+		
+	'''
+    def documentation = 'http://griffon.codehaus.org/Twitter+Plugin'
+}
