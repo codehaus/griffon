e56814852bae8769ea42ea0fc8079b81fe00f229
aucifer_nick <aucifer_nick@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-05-04 20:59:26 +0000

    updated unneeded files and polished dialog up a bit

--- plugins/griffon-airbag/trunk/AirbagGriffonAddon.groovy
+++ plugins/griffon-airbag/trunk/AirbagGriffonAddon.groovy
@@ -1,18 +1,3 @@
-import net.sourceforge.griffon.airbag.AirbagExceptionHandler
-import javax.swing.SwingUtilities
 
 class AirbagGriffonAddon {
-    def events = [
-            "ReadyEnd": {app ->
-                def exceptionHandler = new AirbagExceptionHandler()
-
-                Thread.setDefaultUncaughtExceptionHandler(exceptionHandler)
-
-                System.setProperty("sun.awt.exception.handler", AirbagExceptionHandler.class.getName())
-            },
-
-            "LogC4jonfigStart": {app ->
-                System.setProperty("sun.awt.exception.handler", AirbagExceptionHandler.class.getName())
-            }
-    ]
 }
--- plugins/griffon-airbag/trunk/src/main/net/sourceforge/griffon/airbag/AirBag.groovy
+++
@@ -1,8 +0,0 @@
-package net.sourceforge.griffon.airbag
-
-/**
- * @author Nick Zhu (nzhu@jointsource.com)
- */
-interface AirBag {
-    def deploy(Throwable t);
-}
