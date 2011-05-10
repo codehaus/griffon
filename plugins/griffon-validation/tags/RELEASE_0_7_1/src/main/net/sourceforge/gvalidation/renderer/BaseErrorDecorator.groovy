dbf8a8bc0273ac14671de5d386355f75abf8906b
aucifer_nick <aucifer_nick@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-05-10 21:13:16 +0000

    0.7.1 validation plugin release

---
+++ plugins/griffon-validation/tags/RELEASE_0_7_1/src/main/net/sourceforge/gvalidation/renderer/BaseErrorDecorator.groovy
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2010 the original author or authors.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.sourceforge.gvalidation.renderer
+
+import org.springframework.context.MessageSource
+import net.sourceforge.gvalidation.Errors
+import net.sourceforge.gvalidation.FieldError
+import net.sourceforge.gvalidation.ErrorListener
+import griffon.swing.SwingUtils
+import javax.swing.SwingUtilities
+
+/**
+ * @author Nick Zhu (nzhu@jointsource.com)
+ */
+abstract class BaseErrorDecorator implements ErrorDecorator, ErrorListener {
+
+    protected Object model
+    protected def messageSource
+    protected String errorField
+    protected Object targetComponent
+
+    public void register(model, node, errorField, messageSource){
+        this.model = model
+        this.targetComponent = node
+        this.errorField = errorField
+        this.messageSource = messageSource
+
+        this.model.errors.addListener(this)
+    }
+
+    public boolean isRegistered() {
+        return model != null
+    }
+
+    public String getErrorField() {
+        return errorField
+    }
+
+    def getModel() {
+        return model
+    }
+
+    def getMessageSource() {
+        return messageSource
+    }
+
+    def getTargetComponent() {
+        return targetComponent
+    }
+
+    /**
+     * This abstract method should be implemented by subclasses to perform actual UI
+     * related decoration. This method is guaranteed to be invoked in edt.
+     *
+     * @param errors current errors
+     * @param fieldError associated field error instance
+     */
+    abstract protected void decorate(Errors errors, FieldError fieldError)
+
+    /**
+     * This abstract method should be implemented by subclasses to perform actual UI
+     * related un-decoration. This method is guaranteed to be invoked in edt.
+     */
+    abstract protected void undecorate()
+
+    def onFieldErrorAdded(FieldError error) {
+        if(isNotRelatedError(error))
+            return null
+
+        SwingUtilities.invokeLater({decorate(model.errors, error)} as Runnable)
+    }
+
+    def onFieldErrorRemoved(FieldError error) {
+        if(isNotRelatedError(error))
+            return null
+
+        SwingUtilities.invokeLater({undecorate()} as Runnable)
+    }
+
+    private boolean isNotRelatedError(FieldError error) {
+        return error.field != errorField
+    }
+}
