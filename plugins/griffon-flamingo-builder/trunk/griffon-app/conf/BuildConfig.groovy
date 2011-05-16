23170d6b33f150100c0efc3b43511d1210ecf74b
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-05-16 22:17:15 +0000

    release

--- plugins/griffon-flamingo-builder/trunk/griffon-app/conf/BuildConfig.groovy
+++ plugins/griffon-flamingo-builder/trunk/griffon-app/conf/BuildConfig.groovy
@@ -1,34 +1,18 @@
 griffon.project.dependency.resolution = {
     inherits("global")
-    log "warn"
+    log "warn" 
     repositories {
         griffonPlugins()
         griffonHome()
         griffonCentral()
 
+        flatDir name: 'flamingoBuilderPluginLib', dirs: 'lib'
         mavenCentral()
-        mavenRepo ' https://repository.jboss.org/nexus/content/groups/public-jboss'
+        mavenRepo 'http://repository.codehaus.org'
     }
     dependencies {
-        runtime 'xml-apis:xml-apis-ext:1.3.04',
-                'batik:batik-anim:1.7',
-                'batik:batik-awt-util:1.7',
-                'batik:batik-bridge:1.7',
-                'batik:batik-codec:1.7',
-                'batik:batik-css:1.7',
-                'batik:batik-dom:1.7',
-                'batik:batik-ext:1.7',
-                'batik:batik-extension:1.7',
-                'batik:batik-gui-util:1.7',
-                'batik:batik-gvt:1.7',
-                'batik:batik-parser:1.7',
-                'batik:batik-script:1.7',
-                'batik:batik-svg-dom:1.7',
-                'batik:batik-svggen:1.7',
-                'batik:batik-swing:1.7',
-                'batik:batik-transcoder:1.7',
-                'batik:batik-util:1.7',
-                'batik:batik-xml:1.7'
+        compile 'org.codehaus.griffon:flamingobuilder:0.4'
+                'org.pushingpixels:flamingo:4.1'
     }
 }
 
@@ -36,7 +20,7 @@ griffon {
     doc {
         logo = '<a href="http://griffon.codehaus.org" target="_blank"><img alt="The Griffon Framework" src="../img/griffon.png" border="0"/></a>'
         sponsorLogo = "<br/>"
-        footer = "<br/><br/>Made with Griffon (0.9)"
+        footer = "<br/><br/>Made with Griffon (@griffon.version@)"
     }
 }
 
