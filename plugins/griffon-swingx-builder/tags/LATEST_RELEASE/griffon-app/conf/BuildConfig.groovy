6ee4f5a4cc898742c4abb69bebf69f4e6e3fc9cc
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-05-18 10:39:13 +0000

    griffon-swingx-builder:

---
+++ plugins/griffon-swingx-builder/tags/LATEST_RELEASE/griffon-app/conf/BuildConfig.groovy
@@ -0,0 +1,26 @@
+griffon.project.dependency.resolution = {
+    inherits("global")
+    log "warn"
+    repositories {
+        griffonPlugins()
+        griffonHome()
+        griffonCentral()
+        mavenRepo 'http://repository.codehaus.org'
+        mavenCentral()
+    }
+    dependencies {
+        compile('org.codehaus.griffon:swingxbuilder:0.1.8.1') {
+            excludes 'groovy-all'
+        }
+    }
+}
+
+griffon {
+    doc {
+        logo = '<a href="http://griffon.codehaus.org" target="_blank"><img alt="The Griffon Framework" src="../img/griffon.png" border="0"/></a>'
+        sponsorLogo = "<br/>"
+        footer = "<br/><br/>Made with Griffon (@griffon.version@)"
+    }
+}
+
+griffon.jars.destDir='target/addon'
