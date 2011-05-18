f50dafacd0806d992cc14f0cf3581b2eca0dac84
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-05-18 21:14:51 +0000

    griffon-slideware:

---
+++ plugins/griffon-slideware/tags/LATEST_RELEASE/src/templates/artifacts/Slide.groovy
@@ -0,0 +1,6 @@
+slide(id: 'slide@count@', title: 'slide@count@') {
+    migLayout(layoutConstraints: 'fill',
+              columnConstraints: '[center]',
+              rowConstraints: '[center]')
+    label("Insert your text here")
+}
