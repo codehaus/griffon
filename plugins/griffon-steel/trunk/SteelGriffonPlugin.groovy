cd6e61794b8ea46f84f3f965b1b6434d3653a143
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-06-14 08:53:07 +0000

    griffon-steel:

--- plugins/griffon-steel/trunk/SteelGriffonPlugin.groovy
+++ plugins/griffon-steel/trunk/SteelGriffonPlugin.groovy
@@ -33,7 +33,7 @@
  */
 class SteelGriffonPlugin {
     // the plugin version
-    def version = "0.6"
+    def version = "0.7"
     // the version or versions of Griffon the plugin is designed for
     def griffonVersion = '0.9.2 > *' 
     // the other plugins this plugin depends on
