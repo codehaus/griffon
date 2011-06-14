2df9f82acceec40802caadc5a64131b3b3079568
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-06-14 08:05:22 +0000

    griffon-jcalendar: fix license headers

--- plugins/griffon-jcalendar/trunk/JcalendarGriffonPlugin.groovy
+++ plugins/griffon-jcalendar/trunk/JcalendarGriffonPlugin.groovy
@@ -2,11 +2,9 @@
  * griffon-jcalendar: JCalendar Griffon plugin
  * Copyright 2010 and beyond, Andres Almiray
  *
- * SmartGWT is free software; you can redistribute it and/or modify it
+ * JCalendar is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License version 3
- * as published by the Free Software Foundation.  SmartGWT is also
- * available under typical commercial license terms - see
- * smartclient.com/license.
+ * as published by the Free Software Foundation.
  *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
