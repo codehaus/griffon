874d31a1f054bfa9cf30093a9fe65f585fcc78df
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-06-25 21:13:45 +0000

    release

---
+++ plugins/griffon-lookandfeel-pgslaf/tags/LATEST_RELEASE/griffon-app/conf/BuildConfig.groovy
@@ -0,0 +1,24 @@
+griffon.project.dependency.resolution = {
+    inherits("global")
+    log "warn"
+    repositories {
+        griffonPlugins()
+        griffonHome()
+        griffonCentral()
+        flatDir name: 'lookandfeelPgslafPluginLib', dirs: 'lib'
+    }
+    dependencies {
+        runtime 'com.pagosoft:PgsLookAndFeel:1.1.1',
+                'com.pagosoft:PgsLookAndFeel-jide:1.1.1'
+    }
+}
+
+griffon {
+    doc {
+        logo = '<a href="http://griffon.codehaus.org" target="_blank"><img alt="The Griffon Framework" src="../img/griffon.png" border="0"/></a>'
+        sponsorLogo = "<br/>"
+        footer = "<br/><br/>Made with Griffon (@griffon.version@)"
+    }
+}
+
+griffon.jars.destDir='target/addon'
