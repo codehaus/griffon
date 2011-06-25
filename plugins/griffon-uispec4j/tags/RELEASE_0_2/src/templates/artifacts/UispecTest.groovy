6648db5bcf7e4a642692bf2c97e5d1520affc8c9
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-06-25 20:55:11 +0000

    release

---
+++ plugins/griffon-uispec4j/tags/RELEASE_0_2/src/templates/artifacts/UispecTest.groovy
@@ -0,0 +1,17 @@
+@artifact.package@import org.uispec4j.*
+import griffon.uispec4j.GriffonUISpecTestCase
+
+class @artifact.name@ extends GriffonUISpecTestCase {
+    void testSomething() {
+        // Window window = getMainWindow() 
+    }
+ 
+    // protected void onSetUp() {
+    // }
+ 
+    // protected void onTearDown() {
+    // }
+ 
+    // protected UISpecAdapter initAdapter() {
+    // }
+}
