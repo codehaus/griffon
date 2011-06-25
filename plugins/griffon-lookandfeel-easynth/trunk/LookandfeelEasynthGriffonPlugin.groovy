09c8c5399b39a1b3385d6abd6700e1b9524250f7
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-06-25 20:59:11 +0000

    release

--- plugins/griffon-lookandfeel-easynth/trunk/LookandfeelEasynthGriffonPlugin.groovy
+++ plugins/griffon-lookandfeel-easynth/trunk/LookandfeelEasynthGriffonPlugin.groovy
@@ -18,9 +18,9 @@
  * @author Andres Almiray
  */
 class LookandfeelEasynthGriffonPlugin {
-    def version = 0.2
-    def griffonVersion = '0.9.2 > *'
-    def dependsOn = [lookandfeel: 0.4]
+    def version = 0.3
+    def griffonVersion = '0.9.3 > *'
+    def dependsOn = [lookandfeel: 0.5]
     def toolkits = ['swing']
     def license = 'Apache Software License 2.0'
 
