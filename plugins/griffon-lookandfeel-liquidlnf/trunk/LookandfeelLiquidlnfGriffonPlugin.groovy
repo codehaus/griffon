2bfa51195df21c38857c52e2bce9ca88428dda86
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-06-25 21:06:18 +0000

    release

--- plugins/griffon-lookandfeel-liquidlnf/trunk/LookandfeelLiquidlnfGriffonPlugin.groovy
+++ plugins/griffon-lookandfeel-liquidlnf/trunk/LookandfeelLiquidlnfGriffonPlugin.groovy
@@ -18,10 +18,10 @@
  * @author Andres Almiray
  */
 class LookandfeelLiquidlnfGriffonPlugin {
-    def version = 0.2
-    def dependsOn = [lookandfeel: 0.4]
+    def version = 0.3
+    def dependsOn = [lookandfeel: 0.5]
     def toolkits = ['swing']
-    def griffonVersion = '0.9.2 > *'
+    def griffonVersion = '0.9.3 > *'
     def license = 'Apache Software License 2.0'
 
     def author = 'Andres Almiray'
