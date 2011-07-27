2d533ea4dc1a97c02893ae42c7fe15cfc4e5d077
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-07-27 10:04:37 +0000

    griffon-db4o:

---
+++ plugins/griffon-db4o/tags/RELEASE_0_4/scripts/_Uninstall.groovy
@@ -0,0 +1,35 @@
+/* --------------------------------------------------------------------
+   griffon-db4o plugin
+   Copyright (C) 2010 Andres Almiray
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this library; if not, see <http://www.gnu.org/licenses/>.
+   ---------------------------------------------------------------------
+*/
+
+/**
+ * @author Andres Almiray
+ */
+
+// check to see if we already have a Db4oGriffonAddon
+boolean addonIsSet1
+builderConfig.each() { prefix, v ->
+    v.each { builder, views ->
+        addonIsSet1 = addonIsSet1 || 'Db4oGriffonAddon' == builder
+    }
+}
+
+if(addonIsSet1) {
+    builderConfigFile.text = builderConfigFile.text - "root.'Db4oGriffonAddon'.addon=true\n"
+}
+
---
+++ plugins/griffon-db4o/tags/RELEASE_0_4/src/templates/artifacts/BootstrapDb4o.groovy
@@ -0,0 +1,9 @@
+import com.db4o.ObjectContainer
+
+class BootstrapDb4o {
+    def init = { ObjectContainer db -> 
+    }
+
+    def destroy = { ObjectContainer db ->
+    }
+} 
