234a9ad70426e2e8c6fce2f8e5374062ef81553c
saschaklein <saschaklein@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-08-01 16:25:23 +0000

    Initial release

---
+++ plugins/griffon-resources/tags/LATEST_RELEASE/test/resourceBase/resourcemanager/ResourceManagerTestsResources_de.groovy
@@ -0,0 +1,7 @@
+test {
+    key1 = 'resourcemanager/resources/ResourceManagerTests_de.groovy'
+    key2 = 'resourcemanager/resources/ResourceManagerTests_de.groovy'
+    key3 = 'resourcemanager/resources/ResourceManagerTests_de.groovy'
+    key4 = 'resourcemanager/resources/ResourceManagerTests_de.groovy'
+    key5 = 'resourcemanager/resources/ResourceManagerTests_de.groovy'
+}
