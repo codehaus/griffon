30756f219e5878518ed51a8bba8d93f4100482f8
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-08-08 08:27:40 +0000

    griffon-datasource: initial commit

---
+++ plugins/griffon-datasource/trunk/griffon-app/conf/BuildConfig.groovy
@@ -0,0 +1,25 @@
+griffon.project.dependency.resolution = {
+    inherits "global"
+    log "warn"
+    repositories {
+        griffonPlugins()
+        griffonHome()
+        griffonCentral()
+        mavenCentral()
+    }
+    dependencies {
+        compile('commons-dbcp:commons-dbcp:1.2.2',
+                'commons-pool:commons-pool:1.5.3',
+                'com.h2database:h2:1.3.158') {
+            transitive = false
+        }
+    }
+}
+
+griffon {
+    doc {
+        logo = '<a href="http://griffon.codehaus.org" target="_blank"><img alt="The Griffon Framework" src="../img/griffon.png" border="0"/></a>'
+        sponsorLogo = "<br/>"
+        footer = "<br/><br/>Made with Griffon (@griffon.version@)"
+    }
+}
