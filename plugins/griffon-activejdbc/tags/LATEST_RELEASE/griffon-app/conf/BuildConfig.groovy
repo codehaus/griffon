eb4ef29a0f275c21c477cb79f65ae7e80bbbf1f5
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-08-15 08:43:12 +0000

    release

---
+++ plugins/griffon-activejdbc/tags/LATEST_RELEASE/griffon-app/conf/BuildConfig.groovy
@@ -0,0 +1,34 @@
+griffon.project.dependency.resolution = {
+    // inherit Griffon' default dependencies
+    inherits "global"
+    log "warn" 
+    repositories {
+        griffonPlugins()
+        griffonHome()
+        griffonCentral()
+        mavenCentral()
+        mavenRepo 'http://ipsolutionsdev.com/snapshots/'
+    }
+    
+    def activejdbcVersion = '1.1-SNAPSHOT'
+    
+    dependencies {
+        compile("activejdbc:activejdbc:$activejdbcVersion") {
+            excludes 'junit', 'servlet-api', 'slf4j-api', 'jcl-over-slf4j', 'slf4j-simple'
+        }
+        compile("javalite:javalite-common:1.0") {
+            excludes 'jaxen'
+        }
+        compile("activejdbc:activejdbc-instrumentation:$activejdbcVersion") {
+            transitive = false
+        }
+    }
+}
+
+griffon {
+    doc {
+        logo = '<a href="http://griffon.codehaus.org" target="_blank"><img alt="The Griffon Framework" src="../img/griffon.png" border="0"/></a>'
+        sponsorLogo = "<br/>"
+        footer = "<br/><br/>Made with Griffon (@griffon.version@)"
+    }
+}
