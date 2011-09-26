6306c6a438604687c44c80bb65e014391c773c5a
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-09-26 18:54:06 +0000

    griffon-clojure:

---
+++ plugins/griffon-clojure/tags/RELEASE_0_8/griffon-app/conf/BuildConfig.groovy
@@ -0,0 +1,34 @@
+griffon.project.dependency.resolution = {
+    inherits "global"
+    log "warn"
+    repositories {
+        griffonPlugins()
+        griffonHome()
+        griffonCentral()
+        mavenCentral()
+        mavenRepo 'http://repository.sonatype.org/content/groups/public'
+        mavenRepo 'http://repository.springsource.com/maven/bundles/release'
+    }
+    dependencies {
+        def clojureVersion = '1.3.0'
+        build "org.clojure:clojure:$clojureVersion",
+              "jline:jline:0.9.94",
+              "org.fusesource.jansi:jansi:1.4"
+        compile "org.clojure:clojure:$clojureVersion"
+
+        def springVersion = '3.0.5.RELEASE'
+        compile("org.springframework:org.springframework.core:$springVersion") {
+            transitive = false
+        }
+    }
+}
+
+griffon {
+    doc {
+        logo = '<a href="http://griffon.codehaus.org" target="_blank"><img alt="The Griffon Framework" src="../img/griffon.png" border="0"/></a>'
+        sponsorLogo = "<br/>"
+        footer = "<br/><br/>Made with Griffon (@griffon.version@)"
+    }
+}
+
+griffon.jars.destDir='target/addon'
