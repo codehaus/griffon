db1c304534acc72e078d1b08b685baec841ec4cd
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-10-01 00:01:49 +0000

    griffon-couchdb:

--- plugins/griffon-couchdb/tags/LATEST_RELEASE/src/templates/artifacts/BootstrapCouchdb.groovy
+++
@@ -1,9 +0,0 @@
-import org.jcouchdb.db.Database
-
-class BootstrapCouchdb {
-    def init = { Database db, String databaseName -> 
-    }
-
-    def destroy = { Database db, String databaseName ->
-    }
-} 
