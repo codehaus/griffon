93eabf5830b7330e387e9c16da8bfc26e83800fa
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-10-12 00:25:42 +0000

    griffon-metawidget: initial commit

---
+++ plugins/griffon-metawidget/trunk/src/main/org/metawidget/inspector/impl/propertystyle/javabean/GriffonArtifactPropertyStyle.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+package org.metawidget.inspector.impl.propertystyle.javabean;
+
+import griffon.core.GriffonClass;
+
+/**
+ * @author Andres Almiray
+ */
+public class GriffonArtifactPropertyStyle extends JavaBeanPropertyStyle {
+	/**
+	 * Whether to exclude the given property name when searching for properties.
+	 * <p>
+	 * This can be useful when the convention or base class define properties that are
+	 * framework-specific, and should be filtered out from 'real' business model properties.
+	 * <p>
+	 * By default, excludes GriffonClass.STANDARD_PROPERTIES, 'propertyChangeListeners' and 'vetoableChangeListeners'.
+	 *
+	 * @return true if the property should be excluded, false otherwise
+	 */
+	@Override
+	protected boolean isExcludedName(String name) {
+		if (GriffonClass.STANDARD_PROPERTIES.contains(name)) {
+			return true;
+		}
+
+		return super.isExcludedName(name);
+	}
+}
