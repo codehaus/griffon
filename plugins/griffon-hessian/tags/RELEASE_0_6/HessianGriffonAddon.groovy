80caa56c66c60b87c329426ffa9ad1c0e1b65750
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-10-21 15:59:49 +0000

    release

---
+++ plugins/griffon-hessian/tags/RELEASE_0_6/HessianGriffonAddon.groovy
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2009-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import griffon.plugins.hessian.HessianConnector
+
+/**
+ * @author Andres Almiray
+ */
+class HessianGriffonAddon {
+    def events = [
+        NewInstance: { klass, type, instance ->
+            def types = app.config.griffon?.hessian?.injectInto ?: ['controller']
+            if(!types.contains(type)) return
+            def mc = app.artifactManager.findGriffonClass(klass).metaClass
+            HessianConnector.enhance(mc, instance)
+        }
+    ]
+}
