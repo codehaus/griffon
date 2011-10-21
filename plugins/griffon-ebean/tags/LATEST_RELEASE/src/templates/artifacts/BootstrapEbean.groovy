3f57cd64ef5fd5533b2cb19a39cfab00ad073d73
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-10-21 15:53:00 +0000

    release

--- plugins/griffon-ebean/tags/LATEST_RELEASE/src/templates/artifacts/BootstrapEbean.groovy
+++
@@ -1,9 +0,0 @@
-import com.avaje.ebean.EbeanServer
-
-class BootstrapEbean {
-    def init = { String dataSourceName, EbeanServer ebeanServer ->
-    }
-
-    def destroy = { String dataSourceName, EbeanServer ebeanServer ->
-    }
-} 
