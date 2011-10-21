56abcde4c8796fc031ef0646243c71008ed002f7
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-10-21 16:29:49 +0000

    release

--- plugins/griffon-jmonkeyengine/trunk/JmonkeyengineGriffonPlugin.groovy
+++ plugins/griffon-jmonkeyengine/trunk/JmonkeyengineGriffonPlugin.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2010 the original author or authors.
+ * Copyright 2009-2011 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the 'License');
  * you may not use this file except in compliance with the License.
@@ -18,10 +18,10 @@
  * @author Andres Almiray
  */
 class JmonkeyengineGriffonPlugin {
-    def version = '0.1.1'
+    def version = '0.2'
     def dependsOn = [lwjgl: 0.4]
     def toolkits = ['swing']
-    def griffonVersion = '0.9.2 > *'
+    def griffonVersion = '0.9.4 > *'
     def license = 'Apache Software License 2.0'
 
     def author = 'Andres Almiray'
