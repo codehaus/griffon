ceac4c76004be991b92d9a141b78039446d743d4
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-10-21 15:03:09 +0000

    release

--- plugins/griffon-i18n/trunk/scripts/_Install.groovy
+++ plugins/griffon-i18n/trunk/scripts/_Install.groovy
@@ -19,16 +19,8 @@
  */
 
 // check to see if we already have a I18nGriffonAddon
-boolean addonIsSet1
-builderConfig.each() { prefix, v ->
-    v.each { builder, views ->
-        addonIsSet1 = addonIsSet1 || 'I18nGriffonAddon' == builder
-    }
-}
-
-if (!addonIsSet1) {
+configText = '''root.'I18nGriffonAddon'.addon=true'''
+if(!(builderConfigFile.text.contains(configText))) {
     println 'Adding I18nGriffonAddon to Builder.groovy'
-    builderConfigFile.append('''
-root.'I18nGriffonAddon'.addon=true
-''')
+    builderConfigFile.text += '\n' + configText + '\n'
 }
