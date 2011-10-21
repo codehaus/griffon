6639f6706256f7d700f650f37bc59d449602102c
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-10-21 16:14:59 +0000

    release

--- plugins/griffon-neo4j/tags/LATEST_RELEASE/src/templates/artifacts/BootstrapNeo4j.groovy
+++
@@ -1,9 +0,0 @@
-import org.neo4j.kernel.EmbeddedGraphDatabase
-
-class BootstrapNeo4j {
-    def init = { EmbeddedGraphDatabase db -> 
-    }
-
-    def destroy = { EmbeddedGraphDatabase db ->
-    }
-} 
