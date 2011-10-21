8949d28785259032f99dc3cc48d1db13c71d708d
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-10-21 22:32:05 +0000

    release

---
+++ plugins/griffon-slick/tags/LATEST_RELEASE/griffon-app/conf/BuildConfig.groovy
@@ -0,0 +1,24 @@
+griffon.project.dependency.resolution = {
+    inherits("global")
+    log "warn" 
+    repositories {
+        griffonPlugins()
+        griffonHome()
+        griffonCentral()
+        flatDir name: 'slickPluginLib', dirs: 'lib'
+    }
+    dependencies {
+        compile 'org.newdawn:slick:274',
+                'com.jcraft:jorbis:0.0.17'
+    }
+}
+
+griffon {
+    doc {
+        logo = '<a href="http://griffon.codehaus.org" target="_blank"><img alt="The Griffon Framework" src="../img/griffon.png" border="0"/></a>'
+        sponsorLogo = "<br/>"
+        footer = "<br/><br/>Made with Griffon (@griffon.version@)"
+    }
+}
+
+griffon.jars.destDir='target/addon'
