a0161a1276524fa0b205b20e8ae9c2f8ba713a28
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-10-21 15:13:55 +0000

    release

--- plugins/griffon-mybatis/tags/LATEST_RELEASE/src/templates/artifacts/BootstrapMybatis.groovy
+++
@@ -1,9 +0,0 @@
-import org.apache.ibatis.session.SqlSession
-
-class BootstrapMybatis {
-    def init = { String dataSourceName, SqlSession session ->
-    }
-
-    def destroy = { String dataSourceName, SqlSession session ->
-    }
-} 
--- plugins/griffon-mybatis/tags/LATEST_RELEASE/src/templates/artifacts/MybatisClass.groovy
+++
@@ -1,6 +0,0 @@
-@artifact.package@import java.io.Serializable
-
-class @artifact.name@ implements Serializable {
-    int id
-    String name
-}
