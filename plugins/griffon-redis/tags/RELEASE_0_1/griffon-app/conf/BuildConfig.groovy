8d45ebdb3ea45f43489101427e164ce7df338813
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-10-21 16:11:32 +0000

    release

---
+++ plugins/griffon-redis/tags/RELEASE_0_1/griffon-app/conf/BuildConfig.groovy
@@ -0,0 +1,23 @@
+griffon.project.dependency.resolution = {
+    inherits "global"
+    log "warn"
+    repositories {
+        griffonPlugins()
+        griffonHome()
+        griffonCentral()
+        mavenCentral()
+    }
+    dependencies {
+        compile 'redis.clients:jedis:2.0.0'
+    }
+}
+
+griffon {
+    doc {
+        logo = '<a href="http://griffon.codehaus.org" target="_blank"><img alt="The Griffon Framework" src="../img/griffon.png" border="0"/></a>'
+        sponsorLogo = "<br/>"
+        footer = "<br/><br/>Made with Griffon (@griffon.version@)"
+    }
+}
+
+griffon.jars.destDir='target/addon'
