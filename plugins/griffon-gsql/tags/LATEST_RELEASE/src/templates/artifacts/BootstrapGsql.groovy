a118343bb546b934492b741e64f165c17dfc46c9
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-10-21 15:51:00 +0000

    release

--- plugins/griffon-gsql/tags/LATEST_RELEASE/src/templates/artifacts/BootstrapGsql.groovy
+++
@@ -1,9 +0,0 @@
-import groovy.sql.Sql
-
-class BootstrapGsql {
-    def init = { String dataSourceName = 'default', Sql sql ->
-    }
-
-    def destroy = { String dataSourceName = 'default', Sql sql ->
-    }
-} 
