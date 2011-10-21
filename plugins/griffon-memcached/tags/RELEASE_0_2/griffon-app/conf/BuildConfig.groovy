4e266e67d70a199435e707315f206eeaf476b24c
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-10-21 16:18:53 +0000

    release

---
+++ plugins/griffon-memcached/tags/RELEASE_0_2/griffon-app/conf/BuildConfig.groovy
@@ -0,0 +1,22 @@
+griffon.project.dependency.resolution = {
+    inherits "global"
+    log "warn"
+    repositories {
+        griffonPlugins()
+        griffonHome()
+        griffonCentral()
+        mavenCentral()
+        flatDir name: 'memcachedPluginLib', dirs: 'lib'
+    }
+    dependencies {
+        compile 'net.spy.memcached:spymemcached:2.7.3'
+    }
+}
+
+griffon {
+    doc {
+        logo = '<a href="http://griffon.codehaus.org" target="_blank"><img alt="The Griffon Framework" src="../img/griffon.png" border="0"/></a>'
+        sponsorLogo = "<br/>"
+        footer = "<br/><br/>Made with Griffon (@griffon.version@)"
+    }
+}
---
+++ plugins/griffon-memcached/tags/RELEASE_0_2/src/templates/artifacts/BootstrapMemcached.groovy
@@ -0,0 +1,9 @@
+import net.spy.memcached.MemcachedClient
+
+class BootstrapMemcached {
+    def init = { String clientName, MemcachedClient client ->
+    }
+
+    def destroy = { String clientName, MemcachedClient client ->
+    }
+} 
