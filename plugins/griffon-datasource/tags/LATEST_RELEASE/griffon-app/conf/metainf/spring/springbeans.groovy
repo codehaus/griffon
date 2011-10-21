fd961e1b62c8cfe0fca6bae94f2a93c87e7f739a
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-10-21 15:12:11 +0000

    release

--- plugins/griffon-datasource/tags/LATEST_RELEASE/griffon-app/conf/metainf/spring/springbeans.groovy
+++
@@ -1,7 +0,0 @@
-import griffon.plugins.datasource.DataSourceHolder
-import griffon.spring.factory.support.ObjectFactoryBean
-
-'dataSource'(ObjectFactoryBean) {
-    object = DataSourceHolder.instance.getDataSource('default')
-}
-
