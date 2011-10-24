f53cb2e3c44bb7fc5239b7e66f4e6f9dbc6e901d
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-10-24 01:29:34 +0000

    release

--- plugins/griffon-riak/tags/LATEST_RELEASE/src/templates/artifacts/BootstrapRiak.groovy
+++
@@ -1,9 +0,0 @@
-import com.basho.riak.client.RiakClient
-
-class BootstrapRiak {
-    def init = { String clientName, RiakClient riak -> 
-    }
-
-    def destroy = { String clientName, RiakClient riak ->
-    }
-} 
