598f391147b4e46936ac44861172a9dde62651fd
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-10-24 01:19:19 +0000

    release

---
+++ plugins/griffon-terrastore/tags/RELEASE_0_1/src/templates/artifacts/TerrastoreConfig.groovy
@@ -0,0 +1,19 @@
+client {
+}
+environments {
+    development {
+        client {
+            host = 'http://localhost:8080'
+        }
+    }
+    test {
+        client {
+            host = 'http://localhost:8080'
+        }
+    }
+    production {
+        client {
+            host = 'http://localhost:8080'
+        }
+    }
+}
