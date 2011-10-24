d9c72fc11eb3e5ffc63c607cb8d3f5627c3e31eb
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-10-24 01:19:19 +0000

    release

---
+++ plugins/griffon-terrastore/tags/RELEASE_0_1/src/templates/artifacts/BootstrapTerrastore.groovy
@@ -0,0 +1,9 @@
+import terrastore.client.TerrastoreClient
+
+class BootstrapTerrastore {
+    def init = { String clientName, TerrastoreClient client -> 
+    }
+
+    def destroy = { String clientName, TerrastoreClient client ->
+    }
+} 
