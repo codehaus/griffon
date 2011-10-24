5f42ccee5186129bcdea41363a4249f5f7b507fd
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-10-24 01:28:03 +0000

    release

---
+++ plugins/griffon-db4o/tags/LATEST_RELEASE/src/templates/artifacts/BootstrapDb4o.groovy
@@ -0,0 +1,9 @@
+import com.db4o.ObjectContainer
+
+class BootstrapDb4o {
+    def init = { String dataSourceName, ObjectContainer db -> 
+    }
+
+    def destroy = { String dataSourceName, ObjectContainer db ->
+    }
+} 
