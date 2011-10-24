fa9114fda7c3da8d5aacbea31beedd7f56302337
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-10-24 01:20:51 +0000

    release

--- plugins/griffon-redis/tags/LATEST_RELEASE/src/templates/artifacts/BootstrapRedis.groovy
+++
@@ -1,9 +0,0 @@
-import redis.clients.jedis.Jedis
-
-class BootstrapRedis {
-    def init = { String datasourceName, Jedis jedis -> 
-    }
-
-    def destroy = { String datasourceName, Jedis jedis ->
-    }
-} 
