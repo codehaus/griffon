23efa272e29325b61a5ec45cbe9906f2bc39aa7b
saschaklein <saschaklein@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-11-01 16:46:45 +0000

    CreateSlide fixed

--- plugins/griffon-slideware/tags/LATEST_RELEASE/src/templates/artifacts/Slide.groovy
+++
@@ -1,6 +0,0 @@
-slide(id: '@name@', title: '@name@') {
-    migLayout(layoutConstraints: 'fill',
-              columnConstraints: '[center]',
-              rowConstraints: '[center]')
-    label("Insert your text here")
-}
