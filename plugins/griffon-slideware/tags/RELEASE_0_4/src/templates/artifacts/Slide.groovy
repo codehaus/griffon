d905c369e45e2296d8f458357dec81df9c4038ec
saschaklein <saschaklein@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-11-01 18:53:36 +0000

    Fixed ImagePanel to clear its cache

---
+++ plugins/griffon-slideware/tags/RELEASE_0_4/src/templates/artifacts/Slide.groovy
@@ -0,0 +1,6 @@
+slide(id: '@name@', title: '@name@') {
+    migLayout(layoutConstraints: 'fill',
+              columnConstraints: '[center]',
+              rowConstraints: '[center]')
+    label("Insert your text here")
+}
