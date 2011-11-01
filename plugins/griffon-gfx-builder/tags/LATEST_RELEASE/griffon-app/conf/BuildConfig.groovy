e20d1784c111345d1d1023ba0cff1d55f4e2a086
saschaklein <saschaklein@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-11-01 16:00:11 +0000

    Updated to gfx-builder 0.5.1

---
+++ plugins/griffon-gfx-builder/tags/LATEST_RELEASE/griffon-app/conf/BuildConfig.groovy
@@ -0,0 +1,28 @@
+griffon.project.dependency.resolution = {
+    inherits("global")
+    log "warn"
+    repositories {
+        griffonPlugins()
+        griffonHome()
+        griffonCentral()
+
+        mavenLocal()
+        mavenCentral()
+    }
+    dependencies {
+        compile('org.codehaus.griffon:gfxbuilder-core:0.5.1')   { excludes 'groovy-all' }
+        compile('org.codehaus.griffon:gfxbuilder-svg:0.5.1')    { excludes 'groovy-all' }
+        compile('org.codehaus.griffon:gfxbuilder-swingx:0.5.1') { excludes 'groovy-all' }
+    }
+}
+
+griffon {
+    doc {
+        logo = '<a href="http://griffon.codehaus.org" target="_blank"><img alt="The Griffon Framework" src="../img/griffon.png" border="0"/></a>'
+        sponsorLogo = "<br/>"
+        footer = "<br/><br/>Made with Griffon (0.9)"
+    }
+}
+
+griffon.jars.destDir='target/addon'
+griffon.plugin.pack.additional.sources = ['src/gdsl']
