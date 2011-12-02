85c6a0da3496a81a64cf13898bdeaeceea9b1fc5
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-12-02 21:12:08 +0000

    griffon-cassandra:

--- plugins/griffon-cassandra/tags/LATEST_RELEASE/src/templates/artifacts/BootstrapCassandra.groovy
+++
@@ -1,9 +0,0 @@
-import groovy.sql.Sql
-
-class BootstrapCassandra {
-    def init = { String dataSourceName = 'default', Sql sql ->
-    }
-
-    def destroy = { String dataSourceName = 'default', Sql sql ->
-    }
-} 
