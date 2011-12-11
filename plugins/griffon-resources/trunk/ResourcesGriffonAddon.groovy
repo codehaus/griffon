1d6be72d2b04b653e31039dfd397d0f4fd663ae8
saschaklein <saschaklein@bb902e69-0157-0410-a63b-ba7c3dd83122>
2011-12-11 11:38:24 +0000

    Fixes for 0.9.4

--- plugins/griffon-resources/trunk/ResourcesGriffonAddon.groovy
+++ plugins/griffon-resources/trunk/ResourcesGriffonAddon.groovy
@@ -28,65 +28,18 @@ class ResourcesGriffonAddon {
 
     private GriffonApplication app
 
-    // lifecycle methods
-
-    // called once, after the addon is created
-
-    def addonInit(app) {
-        def basenames = app.config?.resources?.basenames ?: [DEFAULT_I18N_FILE]
-        if (!basenames.contains(DEFAULT_I18N_FILE))
-            basenames = [DEFAULT_I18N_FILE] + basenames
-        resourceManager = new ResourceManager(app)
-        app.metaClass.resourceManager = resourceManager
-        app.metaClass.rm = resourceManager
-        resourceManager.basenames = basenames as ObservableList
-        resourceManager.customSuffixes = (app.config?.resources?.customSuffixes ?: []) as ObservableList
-        resourceManager.resourceSuffix = app.config?.resources?.resourceSuffix ?: 'Resources'
-        resourceManager.locale = app.config?.resources?.locale ?: Locale.default
-        resourceManager.loader = app.config?.resources?.loader ?: ResourceManager.classLoader
-        resourceManager.extension = app.config?.resources?.extension ?: 'groovy'
-        resourceManager.log = app.log
-        this.app = app
-    }
-
-    // called once, after all addons have been inited
-    //def addonPostInit(app) {
-    //}
-
-    // called many times, after creating a builder
-    //def addonBuilderInit(app, builder) {
-    //}
-
-    // called many times, after creating a builder and after
-    // all addons have been inited
-    //def addonBuilderPostInit(app) {
-    //}
-
-    // to add MVC Groups use create-mvc
-    // builder fields, these are added to all builders.
-    // closures can either be literal { it -> println it}
-    // or they can be method closures: this.&method
-
-    // adds methods to all builders
-    //def methods = [
-    //    methodName: { /*Closure*/ }
-    //]
-
-    // adds properties to all builders
-    //def props = [
-    //    propertyName: [
-    //        get: { /* optional getter closure */ },
-    //        set: {val-> /* optional setter closure */ },
-    //  ]
-    //]
-
     // adds new factories to all builders
     def factories = ResourceBuilder.factoryMap
+
+    def addonInit(app) {
+        initResourceManager(app)
+    }
 
     // adds application event handlers
     def events = [
             StartupStart: { app ->
-                app.mvcGroups.each { groupName, group ->
+                initResourceManager(app)
+                app.config.mvcGroups.each { groupName, group ->
                     group.each { k, v ->
                         GriffonClass griffonClass = app.artifactManager.findGriffonClass(v)
                         Class cls = griffonClass?.clazz ?: loadClass(app, v)
@@ -97,28 +50,24 @@ class ResourcesGriffonAddon {
                     }
                 }
             }
-            //    "StartupStart": {app -> /* event handler code */ }
     ]
 
-    // handle synthetic node properties or
-    // intercept existing ones
-    //def attributeDelegates = [
-    //    {builder, node, attributes -> /*handler code*/ }
-    //]
-
-    // called before a node is instantiated
-    //def preInstantiateDelegates = [
-    //    {builder, attributes, value -> /*handler code*/ }
-    //]
-
-    // called after the node was instantiated
-    //def postInstantiateDelegates = [
-    //    {builder, attributes, node -> /*handler code*/ }
-    //]
-
-    // called after the node has been fully
-    // processed, including child content
-    //def postNodeCompletionDelegates = [
-    //    {builder, parent, node -> /*handler code*/ }
-    //]
+    def initResourceManager(app) {
+        if (resourceManager == null) {
+            def basenames = app.config?.resources?.basenames ?: [DEFAULT_I18N_FILE]
+            if (!basenames.contains(DEFAULT_I18N_FILE))
+                basenames = [DEFAULT_I18N_FILE] + basenames
+            resourceManager = new ResourceManager(app)
+            app.metaClass.resourceManager = resourceManager
+            app.metaClass.rm = resourceManager
+            resourceManager.basenames = basenames as ObservableList
+            resourceManager.customSuffixes = (app.config?.resources?.customSuffixes ?: []) as ObservableList
+            resourceManager.resourceSuffix = app.config?.resources?.resourceSuffix ?: 'Resources'
+            resourceManager.locale = app.config?.resources?.locale ?: Locale.default
+            resourceManager.loader = app.config?.resources?.loader ?: ResourceManager.classLoader
+            resourceManager.extension = app.config?.resources?.extension ?: 'groovy'
+            resourceManager.log = app.log
+            this.app = app
+        }
+    }
 }
